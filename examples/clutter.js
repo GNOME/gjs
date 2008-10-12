354bdf53d476aee36245efc10d5f42cd34a64735
Lucas Almeida Rocha <lucasr@src.gnome.org>
2008-10-12 14:24:16 +0000

    Update examples/clutter.js to use test image and connect to button press events for testing.
    svn path=/trunk/; revision=7

--- examples/clutter.js
+++ examples/clutter.js
@@ -4,7 +4,14 @@ Clutter.init(null, null);
 
 let stage = new Clutter.Stage();
 
-let texture = new Clutter.Texture({ filename: '' });
+let texture = new Clutter.Texture({ filename: 'test.jpg',
+                                    reactive: true });
+
+texture.connect('button-press-event',
+                function(o, event) {
+                    log('Clicked!');
+                    return true;
+                });
 
 stage.add_actor(texture);
 stage.show();
