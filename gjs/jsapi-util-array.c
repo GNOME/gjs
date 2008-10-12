ea6f05e92d099dcdad2da1a3e23d6a1f95e18e7a
Havoc Pennington <hp@src.gnome.org>
2008-10-12 18:36:17 +0000

    s/LiTL/litl/g
    svn path=/trunk/; revision=18

--- gjs/jsapi-util-array.c
+++ gjs/jsapi-util-array.c
@@ -1,6 +1,6 @@
 /* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil; -*- */
 /*
- * Copyright (c) 2008  LiTL, LLC
+ * Copyright (c) 2008  litl, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
