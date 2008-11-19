3f54571aca9c6b4cd03c39c4795bf1813b57b219
Owen Taylor <otaylor@src.gnome.org>
2008-11-19 22:30:47 +0000

    Fix style in testEverythingEncapsulated.js
    Fix indentation and add missing 'let'.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=560808
    
    svn path=/trunk/; revision=114

--- test/js/testEverythingEncapsulated.js
+++ test/js/testEverythingEncapsulated.js
@@ -1,19 +1,19 @@
 const Everything = imports.gi.Everything;
 
 function testSimpleBoxed() {
-  simple_boxed = new Everything.TestSimpleBoxedA();
-  simple_boxed.some_int = 42;
-  simple_boxed.some_int8 = 43;
-  simple_boxed.some_double = 42.5;
-  assertEquals(42, simple_boxed.some_int);
-  assertEquals(43, simple_boxed.some_int8);
-  assertEquals(42.5, simple_boxed.some_double);
+    let simple_boxed = new Everything.TestSimpleBoxedA();
+    simple_boxed.some_int = 42;
+    simple_boxed.some_int8 = 43;
+    simple_boxed.some_double = 42.5;
+    assertEquals(42, simple_boxed.some_int);
+    assertEquals(43, simple_boxed.some_int8);
+    assertEquals(42.5, simple_boxed.some_double);
 }
 
 function testBoxed() {
-  boxed = new Everything.TestBoxed();
-  boxed.some_int8 = 42;
-  assertEquals(42, boxed.some_int8);
+    let boxed = new Everything.TestBoxed();
+    boxed.some_int8 = 42;
+    assertEquals(42, boxed.some_int8);
 }
 
 gjstestRun();
