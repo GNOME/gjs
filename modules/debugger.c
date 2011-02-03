665f2835e77e60078a7a87a93b041aa62995ecea
Joe Shaw <joeshaw@litl.com>
2011-02-03 11:39:06 -0500

    bring back debugger module, with a nativeTrap() which calls G_BREAKPOINT()
    Makes it easier to debug code which crosses between JS and C by
    firing traps (to be caught in gdb) from JS code.

---
+++ modules/debugger.c
@@ -0,0 +1,55 @@
+/* -*- mode: C; c-basic-offset: 4; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright (c) 2011  litl, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <string.h>
+
+#include <jsapi.h>
+
+#include <glib.h>
+#include <gjs/gjs.h>
+
+#include "debugger.h"
+
+static JSBool
+gjs_debugger_native_trap(JSContext *cx, JSObject *obj, uintN argc,
+                         jsval *argv, jsval *retval)
+{
+    G_BREAKPOINT();
+
+    return JS_TRUE;
+}
+
+JSBool
+gjs_define_debugger_stuff(JSContext *context,
+                          JSObject  *module_obj)
+{
+    if (!JS_DefineFunction(context, module_obj,
+                           "nativeTrap",
+                           gjs_debugger_native_trap,
+                           1, GJS_MODULE_PROP_FLAGS))
+        return JS_FALSE;
+
+    return JS_TRUE;
+}
+
+GJS_REGISTER_NATIVE_MODULE("debugger", gjs_define_debugger_stuff);
