a3e55cdc9f1fdd978b87a27d7d6532bf25e12f24
Eduardo Lima Mitev <elima@igalia.com>
2011-02-23 19:27:57 +0100

    [examples] Replaces deprecated open() by load_uri() in webkit example
    Method webkit_web_view_open() has been deprecated in favor of
    webkit_web_view_load_uri() since version 1.1.1.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=643109

--- examples/webkit.js
+++ examples/webkit.js
@@ -9,7 +9,7 @@ let sw = new Gtk.ScrolledWindow({});
 win.add(sw);
 
 let view = new WebKit.WebView();
-view.open("http://www.google.com/");
+view.load_uri("http://www.google.com/");
 sw.add(view);
 
 win.set_size_request(640, 480);
