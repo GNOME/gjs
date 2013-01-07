d03c9ab24eeb67390005d0d8d2daf16639dacf6a
Tim Lunn <tim@feathertop.org>
2013-01-08 07:51:05 +1100

    testBoxed: add test for getter
    https://bugzilla.gnome.org/show_bug.cgi?id=690982

--- test/js/test0030basicBoxed.js
+++ test/js/test0030basicBoxed.js
@@ -4,6 +4,7 @@ var Regress = imports.gi.Regress;
 function testBasicBoxed() {
     var a = new Regress.TestSimpleBoxedA();
     a.some_int = 42;
+    assertEquals(a.some_int, 42);
 }
 
 gjstestRun();
