e2b52f5e75c5cc5e5b1ca652a08f430457158c39
Giovanni Campagna <gcampagna@src.gnome.org>
2013-04-19 20:27:07 +0200

    FIXME function: introduce a new system for better caching of marshalling info
    The marshalling metadata, as stored in the typelib, is not a format
    friendly to gjs, and requires to have multiple switches and a complex
    logic in the hot path of function calls.
    Instead, extend the current caching mechanism to allow arbitrary
    data to be attached to arguments, and to have it deal with JS arguments,
    rather than a mixture of C and GI arguments.
    
    FIXME: Contains a use-after-free somewhere.
    
    (Philip Chimento: rebased and fixed coding style.)

---
+++ gi/arg-cache.h
@@ -0,0 +1,74 @@
+/* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright (c) 2013 Giovanni Campagna <scampa.giovanni@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef GJS_ARG_CACHE_H
+#define GJS_ARG_CACHE_H
+
+#include <glib.h>
+#include <girepository.h>
+
+#include "function.h"
+#include "gjs/jsapi-wrapper.h"
+
+G_BEGIN_DECLS
+
+typedef struct _GjsArgumentCache {
+    bool (*marshal) (struct _GjsArgumentCache *, GIArgument *, JS::HandleValue);
+    bool (*release) (struct _GjsArgumentCache *, GIArgument *);
+    bool (*free) (struct _GjsArgumentCache *);
+
+    /* For compatibility */
+    GjsParamType param_type;
+
+    union {
+        int dummy;
+    } contents;
+} GjsArgumentCache;
+
+bool gjs_arg_cache_build_in_arg(GjsArgumentCache *self,
+                                GjsParamType     *param_types,
+                                int               gi_index,
+                                GIArgInfo        *arg,
+                                bool&             inc_counter);
+
+bool gjs_arg_cache_build_out_arg(GjsArgumentCache *self,
+                                 GjsParamType     *param_types,
+                                 int               gi_index,
+                                 GIArgInfo        *arg,
+                                 bool&             inc_counter);
+
+bool gjs_arg_cache_build_inout_arg(GjsArgumentCache *in_self,
+                                   GjsArgumentCache *out_self,
+                                   GjsParamType     *param_types,
+                                   int               gi_index,
+                                   GIArgInfo        *arg,
+                                   bool&             inc_counter);
+
+bool gjs_arg_cache_build_return(GjsArgumentCache *self,
+                                GjsParamType     *param_types,
+                                GICallableInfo   *info,
+                                bool&             inc_counter);
+
+G_END_DECLS
+
+#endif  /* GJS_ARG_CACHE_H */
