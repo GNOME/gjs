e9570cf02ffdcfd00846aeff6578feb0e42d4b54
Giovanni Campagna <gcampagna@src.gnome.org>
2013-04-22 18:28:52 +0200

    arg-cache: extend to handle interface types too
    Handle enums, objects and boxed types through the argument cache
    rather than using the generic marshaller.

--- gi/arg-cache.h
+++ gi/arg-cache.h
@@ -61,22 +61,44 @@ typedef struct _GjsArgumentCache {
     bool is_return : 1;
 
     union {
+        /* for explicit array only */
         struct {
             int length_pos;
             GITypeTag length_tag;
         } array;
+
         struct {
             GIScopeType scope;
             int closure_pos;
             int destroy_pos;
         } callback;
+
         struct {
             GITypeTag number_tag;
             unsigned is_unsigned : 1;
         } number;
-        size_t caller_allocates_size;
+
+        /* boxed / union / GObject */
+        struct {
+            GType gtype;
+            GIBaseInfo *info;
+        } object;
+
+        /* foreign structures */
+        GIStructInfo *tmp_foreign_info;
+
+        /* enum / flags */
+        struct {
+            int64_t enum_min;
+            int64_t enum_max;
+        } enum_type;
+        uint64_t flags_mask;
+
+        /* string / filename */
         unsigned string_is_filename : 1;
-        int dummy;
+
+        /* out caller allocates (FIXME: should be in object) */
+        size_t caller_allocates_size;
     } contents;
 } GjsArgumentCache;
 
