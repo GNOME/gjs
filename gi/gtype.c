GNOME/gjs

commit df28a9c070459cc90b0ea0db19bf8e0568ac396a

gtype: Don't recurse infinitely while getting a GType
If we pass a plain old JS object to gjs_gtype_get_actual_gtype,
it will recurse infinitely, getting the constructor. Any object's
constructor is a Function, and unfortunately Function.prototype.constructor
returns itself. Fix this by only recursing once.

https://bugzilla.gnome.org/show_bug.cgi?id=685436

gi/gtype.c
@@ -145,9 +145,10 @@ gjs_gtype_create_gtype_wrapper (JSContext *context,
     return object;
 }
 
-GType
-gjs_gtype_get_actual_gtype (JSContext *context,
-                            JSObject  *object)
+static GType
+_gjs_gtype_get_actual_gtype (JSContext *context,
+                             JSObject  *object,
+                             gboolean   recurse)
 {
     GType gtype = G_TYPE_INVALID;
     jsval gtype_val = JSVAL_VOID;
@@ -162,17 +163,23 @@ gjs_gtype_get_actual_gtype (JSContext *context,
      * property on that and hope it's a GType wrapper object */
     if (!JS_GetProperty(context, object, "$gtype", &gtype_val) ||
         !JSVAL_IS_OBJECT(gtype_val)) {
-
         /* OK, so we're not a class. But maybe we're an instance. Check
-           for "constructor" and recurse on that. */
+         * for "constructor" and recurse on that. */
         if (!JS_GetProperty(context, object, "constructor", &gtype_val))
             goto out;
     }
 
-    if (JSVAL_IS_OBJECT(gtype_val))
-        gtype = gjs_gtype_get_actual_gtype(context, JSVAL_TO_OBJECT(gtype_val));
+    if (recurse && JSVAL_IS_OBJECT(gtype_val))
+        gtype = _gjs_gtype_get_actual_gtype(context, JSVAL_TO_OBJECT(gtype_val), FALSE);
 
  out:
     JS_EndRequest(context);
     return gtype;
+}
+
+GType
+gjs_gtype_get_actual_gtype (JSContext *context,
+                            JSObject  *object)
+{
+    return _gjs_gtype_get_actual_gtype(context, object, TRUE);
 }

commit 84a0a8e92b5f8ffa2ae0f0c00e2841bbeb1671ad

Fix regression retriving GTypes from instances
The GValue marshalling code calls get_actual_gtype() on instances.
To handle that, we need three levels of recursion: from the
instance to the constructor, from the constructor to the GType
object and finally from GType object to the actual GType.

This regressed in df28a9c070459cc90b0ea0db19bf8e0568ac396a,
which was later reverted, but that left the original bug unfixed.

https://bugzilla.gnome.org/show_bug.cgi?id=699878

gi/gtype.c
@@ -145,9 +145,10 @@ gjs_gtype_create_gtype_wrapper (JSContext *context,
     return object;
 }
 
-GType
-gjs_gtype_get_actual_gtype (JSContext *context,
-                            JSObject  *object)
+static GType
+_gjs_gtype_get_actual_gtype (JSContext *context,
+                             JSObject  *object,
+                             int        recurse)
 {
     GType gtype = G_TYPE_INVALID;
     jsval gtype_val = JSVAL_VOID;
@@ -169,10 +170,25 @@ gjs_gtype_get_actual_gtype (JSContext *context,
             goto out;
     }
 
-    if (JSVAL_IS_OBJECT(gtype_val))
-        gtype = gjs_gtype_get_actual_gtype(context, JSVAL_TO_OBJECT(gtype_val));
+    if (recurse > 0 && JSVAL_IS_OBJECT(gtype_val))
+        gtype = _gjs_gtype_get_actual_gtype(context, JSVAL_TO_OBJECT(gtype_val), recurse - 1);
 
  out:
     JS_EndRequest(context);
     return gtype;
+}
+
+GType
+gjs_gtype_get_actual_gtype (JSContext *context,
+                            JSObject  *object)
+{
+    /* 2 means: recurse at most three times (including this
+       call).
+       The levels are calculated considering that, in the
+       worst case we need to go from instance to class, from
+       class to GType object and from GType object to
+       GType value.
+     */
+
+    return _gjs_gtype_get_actual_gtype(context, object, 2);
 }