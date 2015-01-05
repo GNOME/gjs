998507942ac4c7718f4ab2fed9fb802966bf5681
Sam Spilsbury <smspillaz@gmail.com>
2015-01-05 09:25:35 +0800

    Write function line number before unique key in FN entries.
    Before we were not writing the line number before the unique key
    but instead as part of the unique key. This is technically not
    compliant with the lcov specification, which requires th
    following:
    
        FN:<line number>,<function name>
    
    For some reason, genhtml never complained about this and silently
    just accepted the function names. Other tools that work with lcov
    data, like lcov_cobertura and lcov-result-merge are far less
    forgiving.
    
    Fixes BGO #742362

--- gjs/coverage.cpp
+++ gjs/coverage.cpp
@@ -70,6 +70,7 @@ typedef struct _GjsCoverageBranch {
 
 typedef struct _GjsCoverageFunction {
     char         *key;
+    unsigned int line_number;
     unsigned int hit_count;
 } GjsCoverageFunction;
 
@@ -150,6 +151,8 @@ write_function_foreach_func(gpointer value,
     GjsCoverageFunction *function = (GjsCoverageFunction *) value;
 
     write_string_into_stream(stream, "FN:");
+    write_uint32_into_stream(stream, function->line_number);
+    write_string_into_stream(stream, ",");
     write_string_into_stream(stream, function->key);
     write_string_into_stream(stream, "\n");
 }
@@ -561,9 +564,11 @@ get_executed_lines_for(GjsCoverage *coverage,
 static void
 init_covered_function(GjsCoverageFunction *function,
                       char                *key,
+                      unsigned int        line_number,
                       unsigned int        hit_count)
 {
     function->key = key;
+    function->line_number = line_number;
     function->hit_count = hit_count;
 }
 
@@ -616,12 +621,21 @@ convert_and_insert_function_decl(GArray    *array,
         return FALSE;
     }
 
-    unsigned int line_number = JSVAL_TO_INT(hit_count_property_value);
+    jsval line_number_property_value;
+    if (!JS_GetProperty(context, object, "line", &line_number_property_value) ||
+        !JSVAL_IS_INT(line_number_property_value)) {
+        gjs_throw(context, "Failed to get line property for function object");
+        return FALSE;
+    }
+
+    unsigned int line_number = JSVAL_TO_INT(line_number_property_value);
+    unsigned int hit_count = JSVAL_TO_INT(hit_count_property_value);
 
     GjsCoverageFunction info;
     init_covered_function(&info,
                           utf8_string,
-                          line_number);
+                          line_number,
+                          hit_count);
 
     g_array_append_val(array, info);
 
