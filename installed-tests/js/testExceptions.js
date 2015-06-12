7a8496400a3d07963df25cdfdbc74991db75b107
Cosimo Cecchi <cosimoc@gnome.org>
2015-06-11 18:12:25 -0700

    installed-tests: add test for exceptions inside gobject properties
    Add a test for the previous commit.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=730101

---
+++ installed-tests/js/testExceptions.js
@@ -0,0 +1,101 @@
+const JSUnit = imports.jsUnit;
+const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
+const Lang = imports.lang;
+
+const Foo = new Lang.Class({
+    Name: 'Foo',
+    Extends: GObject.Object,
+    Properties: {
+        'prop': GObject.ParamSpec.string('prop', '', '', GObject.ParamFlags.READWRITE, '')
+    },
+
+    set prop(v) {
+	throw new Error('set');
+    },
+
+    get prop() {
+	throw new Error('get');
+    }
+});
+
+const Bar = new Lang.Class({
+    Name: 'Bar',
+    Extends: GObject.Object,
+    Properties: {
+        'prop': GObject.ParamSpec.string('prop', '', '', GObject.ParamFlags.READWRITE, '')
+    }
+});
+
+function testExceptionInPropertySetter() {
+    let foo = new Foo();
+    GLib.test_expect_message('Gjs', GLib.LogLevelFlags.LEVEL_WARNING,
+                             'JS ERROR: Error: set*');
+
+    try {
+	foo.prop = 'bar';
+    } catch (e) {
+	logError(e);
+    }
+}
+
+function testExceptionInPropertyGetter() {
+    let foo = new Foo();
+    GLib.test_expect_message('Gjs', GLib.LogLevelFlags.LEVEL_WARNING,
+                             'JS ERROR: Error: get*');
+
+    try {
+	let bar = foo.prop;
+    } catch (e) {
+	logError(e);
+    }
+}
+
+function testExceptionInPropertySetterFromConstructor() {
+    GLib.test_expect_message('Gjs', GLib.LogLevelFlags.LEVEL_WARNING,
+                             'JS ERROR: Error: set*');
+
+    try {
+	let foo = new Foo({ prop: 'bar' });
+    } catch (e) {
+	logError(e);
+    }
+}
+
+function testExceptionInPropertySetterWithBinding() {
+    let foo = new Foo();
+    let bar = new Bar();
+
+    bar.bind_property('prop',
+		      foo, 'prop',
+		      GObject.BindingFlags.DEFAULT);
+    GLib.test_expect_message('Gjs', GLib.LogLevelFlags.LEVEL_WARNING,
+                             'JS ERROR: Error: set*');
+
+    try {
+	// wake up the binding so that g_object_set() is called on foo
+	bar.notify('prop');
+    } catch (e) {
+	logError(e);
+    }
+}
+
+function testExceptionInPropertyGetterWithBinding() {
+    let foo = new Foo();
+    let bar = new Bar();
+
+    foo.bind_property('prop',
+		      bar, 'prop',
+		      GObject.BindingFlags.DEFAULT);
+    GLib.test_expect_message('Gjs', GLib.LogLevelFlags.LEVEL_WARNING,
+                             'JS ERROR: Error: get*');
+
+    try {
+	// wake up the binding so that g_object_get() is called on foo
+	foo.notify('prop');
+    } catch (e) {
+	logError(e);
+    }
+}
+
+JSUnit.gjstestRun(this, JSUnit.setUp, JSUnit.tearDown);
