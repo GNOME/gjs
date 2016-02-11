f1ec42efbcc4a114033798920e17a8824ca782ef
Michael Catanzaro <mcatanzaro@igalia.com>
2016-02-10 23:05:42 -0600

    examples: Update WebKit example

--- examples/webkit.js
+++ examples/webkit.js
@@ -1,16 +1,13 @@
 const Gtk = imports.gi.Gtk;
-const WebKit = imports.gi.WebKit;
+const WebKit = imports.gi.WebKit2;
 
 Gtk.init(null);
 
 let win = new Gtk.Window();
 
-let sw = new Gtk.ScrolledWindow({});
-win.add(sw);
-
 let view = new WebKit.WebView();
 view.load_uri("http://www.google.com/");
-sw.add(view);
+win.add(view);
 
 win.set_size_request(640, 480);
 win.show_all();
