6137516b1bdb40401f73dc699f9ce72db8340be0
Claudio André <claudioandre.br@gmail.com>
2017-01-03 11:37:04 -0200

    CI: add clang and small tweaks

--- continuous-integration/check.sh
+++ continuous-integration/check.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -e
 
 # Get and install dependencies
-echo
-echo '-- Prepare --'
-echo "Running on: $DISTRO $OS"
 cd /cwd
 
-if [[ $DISTRO == "ubuntu" ]]; then
+if [[ $BASE == "ubuntu" ]]; then
     apt-get update -qq
     apt-get -y install build-essential
     apt-get -y install libmount-dev libpcre3-dev python-dev dh-autoreconf flex bison pkg-config zlib1g-dev libffi-dev
-    apt-get -y install libxml2-utils libgamin-dev
+    apt-get -y install libxml2-utils libgamin-dev xvfb clang
     apt-get -y install software-properties-common
     add-apt-repository -y ppa:ricotz/testing
     apt-get update
@@ -18,6 +15,12 @@ if [[ $DISTRO == "ubuntu" ]]; then
 else
     exit 1
 fi
+
+# Show some environment info
+echo
+echo '-- Environment --'
+echo "Running on: $BASE $OS"
+$CC --version
 
 # Build glib 2.0
 echo
@@ -39,11 +42,14 @@ make install
 echo
 echo '-- gjs build --'
 cd ../gjs
-./autogen.sh --prefix=/usr/local
+./autogen.sh --enable-Werror --enable-installed-tests --with-xvfb-tests # --fPIC needed for clang 3.8.1
 make -sj2
 make install
 
 # Test the build
 echo
 echo '-- Testing GJS --'
-ls -la /usr/local/libexec/gjs
+ls -la /usr/local/libexec/gjs/installed-tests
+#make check  ==> make check fails, # ERROR: 2
+#gnome-desktop-testing-runner gjs ==> Is this what I have to run?
+
