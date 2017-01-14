GNOME/gjs

commit a2fdf358207d075e9cbc6d78895f87b4e5a72bea

js: Use JS_NewObjectWithGivenProto
In mozjs38, JS_NewObject() doesn't take a prototype JSObject anymore;
instead, JS_NewObjectWithGivenProto() is used for that purpose. The
behaviour of JS_NewObject() is changed so that it looks for a default
prototype, which is the same as passing JS::NullPtr() as the proto
parameter in mozjs31.

https://bugzilla.gnome.org/show_bug.cgi?id=776966

gjs/jsapi-dynamic-class.cpp
@@ -84,7 +84,7 @@ gjs_init_class_dynamic(JSContext              *context,
      * constructor is not found (and it won't be found, because we never call
      * JS_InitClass).
      */
-    prototype.set(JS_NewObject(context, clasp, parent_proto, global));
+    prototype.set(JS_NewObjectWithGivenProto(context, clasp, parent_proto, global));
     if (!prototype)
         goto out;
 

gjs/byteArray.cpp
@@ -531,7 +531,7 @@ byte_array_new(JSContext *context)
 
     JS::RootedObject proto(context, byte_array_get_prototype(context));
     JS::RootedObject array(context,
-        JS_NewObject(context, &gjs_byte_array_class, proto, JS::NullPtr()));
+        JS_NewObjectWithGivenProto(context, &gjs_byte_array_class, proto, JS::NullPtr()));
 
     priv = g_slice_new0(ByteArrayInstance);
 
@@ -741,7 +741,7 @@ gjs_byte_array_from_byte_array (JSContext *context,
 
     JS::RootedObject proto(context, byte_array_get_prototype(context));
     JS::RootedObject object(context,
-        JS_NewObject(context, &gjs_byte_array_class, proto, JS::NullPtr()));
+        JS_NewObjectWithGivenProto(context, &gjs_byte_array_class, proto, JS::NullPtr()));
 
     if (!object) {
         gjs_throw(context, "failed to create byte array");

commit 8d85c940d6a275c53fca98f4697eed3112d64e30

dynamic class: Check if prototype is NULL
This fixes a regression introduced in commit
a2fdf358207d075e9cbc6d78895f87b4e5a72bea.

If the prototype passed to the dynamic class's constructor is NULL, then
we have to call JS_NewObject() to get a default prototype, instead of
JS_NewObjectWithGivenProto(). Otherwise the object has _no_ prototype at
all.

Includes a regression test that would have caught this bug.

https://bugzilla.gnome.org/show_bug.cgi?id=776966

gjs/jsapi-dynamic-class.cpp
@@ -77,14 +77,18 @@ gjs_init_class_dynamic(JSContext              *context,
          JS_NewObjectForConstructor can find it
     */
 
-    /*
-     * JS_NewObject will try to search for clasp prototype in the global
-     * object if parent_proto is NULL, which is wrong, but it's not
-     * a problem because it will fallback to Object.prototype if the clasp's
-     * constructor is not found (and it won't be found, because we never call
-     * JS_InitClass).
-     */
-    prototype.set(JS_NewObjectWithGivenProto(context, clasp, parent_proto, global));
+    if (parent_proto) {
+        prototype.set(JS_NewObjectWithGivenProto(context, clasp,
+                                                 parent_proto, global));
+    } else {
+        /* JS_NewObject will try to search for clasp prototype in the
+         * global object, which is wrong, but it's not a problem because
+         * it will fallback to Object.prototype if the clasp's
+         * constructor is not found (and it won't be found, because we
+         * never call JS_InitClass).
+         */
+        prototype.set(JS_NewObject(context, clasp, JS::NullPtr(), global));
+    }
     if (!prototype)
         goto out;
 

installed-tests/js/testEverythingEncapsulated.js
@@ -224,4 +224,9 @@ describe('Introspected GObject', function () {
         'use strict';
         expect(() => obj.some_int8 = 41).toThrow();
     });
+
+    it('has normal Object methods', function () {
+        obj.ownprop = 'foo';
+        expect(obj.hasOwnProperty('ownprop')).toBeTruthy();
+    });
 });