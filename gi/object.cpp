85f88a18faef28cd5ec09817d048082ec1f6bd97
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-11-27 17:22:33 -0500

    object: check in gjs_typecheck_object if the object has been finalized

--- gi/object.cpp
+++ gi/object.cpp
@@ -72,6 +72,7 @@ struct ObjectInstance {
     std::deque<GjsCallbackTrampoline *> vfuncs;
 
     unsigned js_object_finalized : 1;
+    unsigned g_object_finalized  : 1;
 };
 
 static std::stack<JS::PersistentRootedObject> object_init_list;
@@ -938,7 +939,10 @@ static void
 wrapped_gobj_dispose_notify(gpointer      data,
                             GObject      *where_the_object_was)
 {
-    weak_pointer_list.erase(static_cast<ObjectInstance *>(data));
+    ObjectInstance *priv = static_cast<ObjectInstance *>(data);
+
+    priv->g_object_finalized = true;
+    weak_pointer_list.erase(priv);
 #if DEBUG_DISPOSE
     gjs_debug(GJS_DEBUG_GOBJECT, "Wrapped GObject %p disposed", where_the_object_was);
 #endif
@@ -2121,6 +2125,17 @@ gjs_typecheck_object(JSContext       *context,
         if (throw_error) {
             gjs_throw(context,
                       "Object is %s.%s.prototype, not an object instance - cannot convert to GObject*",
+                      priv->info ? g_base_info_get_namespace( (GIBaseInfo*) priv->info) : "",
+                      priv->info ? g_base_info_get_name( (GIBaseInfo*) priv->info) : g_type_name(priv->gtype));
+        }
+
+        return false;
+    }
+
+    if (priv->g_object_finalized) {
+        if (throw_error) {
+            gjs_throw(context,
+                      "Object is %s.%s.prototype, has been already deallocated - cannot convert to GObject*",
                       priv->info ? g_base_info_get_namespace( (GIBaseInfo*) priv->info) : "",
                       priv->info ? g_base_info_get_name( (GIBaseInfo*) priv->info) : g_type_name(priv->gtype));
         }
