4850a1c14a23a60fb5d5a8afde34eda34e5f398c
Claudio André <claudioandre.br@gmail.com>
2017-12-12 09:38:52 -0200

    CI: tweak the docker image
    - Clean up the docker image a bit more.
    - Install lcov in the base docker image.
    [skip ci]

--- test/extra/do_docker.sh
+++ test/extra/do_docker.sh
@@ -8,6 +8,7 @@ function do_Shrink_Image(){
     rm -rf ~/bin/jhbuild ~/.local/bin/jhbuild ~/.cache/jhbuild
     rm -rf ~/.config/jhbuildrc ~/.jhbuildrc ~/checkout
     rm -rf ~/jhbuild/checkout
+    rm -rf ~/jhbuild/install/lib/libjs_static.ajs
 
     if [[ $BASE == "ubuntu" ]]; then
         apt-get -y -qq remove --purge apt-file
