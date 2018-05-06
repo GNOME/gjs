fa92f8a42e9b44f60ad41d21a470fa323ca8487f
Claudio André <claudioandre.br@gmail.com>
2018-05-06 20:19:51 -0300

    CI: build Docker images

--- test/extra/do_basic.sh
+++ test/extra/do_basic.sh
@@ -1,27 +1,5 @@
 #!/bin/bash -e
 
-function do_Install_Git(){
-    echo
-    echo '-- Installing Git --'
-
-    if [[ $BASE == "debian" ]]; then
-        apt-get update -qq
-
-        # Git
-        apt-get -y -qq install git > /dev/null
-
-    elif [[ $BASE == "fedora" ]]; then
-        dnf -y -q upgrade
-
-        # git
-        dnf -y -q install git
-    else
-        echo
-        echo '-- Error: invalid BASE code --'
-        exit 1
-    fi
-}
-
 function do_Install_Base_Dependencies(){
     echo
     echo '-- Installing Base Dependencies --'
@@ -36,9 +14,7 @@ function do_Install_Base_Dependencies(){
                                docbook docbook-xsl libtext-csv-perl \
                                zlib1g-dev libdbus-glib-1-dev \
                                libtool libicu-dev libnspr4-dev \
-                               policykit-1 cppcheck \
-                               apt-file > /dev/null
-        apt-file update
+                               policykit-1 > /dev/null
 
     elif [[ $BASE == "fedora" ]]; then
         dnf -y -q upgrade
@@ -47,7 +23,7 @@ function do_Install_Base_Dependencies(){
         dnf -y -q install @c-development @development-tools clang redhat-rpm-config gnome-common python-devel \
                           pygobject2 dbus-python perl-Text-CSV perl-XML-Parser gettext-devel gtk-doc ninja-build \
                           zlib-devel libffi-devel \
-                          libtool libicu-devel nspr-devel cppcheck
+                          libtool libicu-devel nspr-devel
     else
         echo
         echo '-- Error: invalid BASE code --'
@@ -62,14 +38,18 @@ function do_Install_Dependencies(){
     if [[ $BASE == "debian" ]]; then
         # Testing dependencies
         apt-get -y -qq install libgtk-3-dev gir1.2-gtk-3.0 xvfb gnome-desktop-testing dbus-x11 dbus \
-                               libedit-dev > /dev/null
+                               libedit-dev libgl1-mesa-dev > /dev/null
 
     elif [[ $BASE == "fedora" ]]; then
         # Testing dependencies
         dnf -y -q install gtk3 gtk3-devel gobject-introspection Xvfb gnome-desktop-testing dbus-x11 dbus \
                           cairo intltool libxslt bison nspr zlib python3-devel dbus-glib libicu libffi pcre libxml2 libxslt libtool flex \
                           cairo-devel zlib-devel libffi-devel pcre-devel libxml2-devel libxslt-devel \
-                          libedit-devel libasan libubsan lcov
+                          libedit-devel libasan libubsan lcov mesa-libGL-devel
+
+        if [[ $DEV == "devel" ]]; then
+            dnf -y -q install time
+        fi
     fi
 }
 
@@ -84,5 +64,24 @@ function do_Install_Extras(){
     elif [[ $BASE == "fedora" ]]; then
         # Distros development versions of needed libraries
         dnf -y -q install gobject-introspection-devel
+        dnf -y -q install mesa-dri-drivers ### TODO temporary workaround for a bug in Fedora itself
+
+        # Distros development versions of needed libraries
+        dnf -y debuginfo-install glib2-devel gobject-introspection-devel gtk3-devel expat fontconfig cairo
+    fi
+}
+
+function do_Install_Analyser(){
+    echo
+    echo '-- Installing Static Analysers --'
+    mkdir -p /cwd
+
+    if [[ $BASE == "fedora" ]]; then
+        # Static analysers
+        dnf -y -q install git cppcheck tokei nodejs python-devel
+
+        # Install needed packages
+        pip install cpplint
+        npm install -g eslint
     fi
 }
--- test/extra/do_mozilla.sh
+++ test/extra/do_mozilla.sh
@@ -1,8 +1,30 @@
 #!/bin/bash -e
 
+function do_Configure_MozBuild(){
+    echo
+    echo '-- Set JHBuild Configuration --'
+
+    mkdir -p ~/.config
+    autogenargs=""
+
+    if [[ -n "${BUILD_OPTS}" ]]; then
+        autogenargs="$autogenargs $BUILD_OPTS"
+    fi
+
+    cat <<EOFILE >> ~/.config/jhbuildrc
+module_autogenargs['mozjs52'] = "$autogenargs"
+module_makeargs['mozjs52'] = '-s'
+EOFILE
+
+    echo '-- Done --'
+}
+
 function do_Build_Mozilla(){
     echo
     echo '-- Building Mozilla SpiderMonkey --'
+
+    # Configure the Mozilla build
+    do_Configure_MozBuild
 
     # Build Mozilla Stuff
     jhbuild build mozjs52
