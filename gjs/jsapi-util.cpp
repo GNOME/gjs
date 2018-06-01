2929a3bd13394b22dc596e2bcc02b4dc4ab1140d
John Renner <john@jrenner.net>
2018-05-31 20:57:36 -0700

    Make module evaluation opt-in

--- gjs/jsapi-util.cpp
+++ gjs/jsapi-util.cpp
@@ -839,20 +839,7 @@ gjs_eval_with_scope(JSContext             *context,
     if (!scope_chain.append(eval_obj))
         g_error("Unable to append to vector");
 
-    // FIXME: This can't stay because loading scripts as modules forces strict mode
-    // This breaks a lot of current files.
-    JS::RootedObject module(context);
-    if(!CompileModule(context, options, buf, &module)) {
-        return false;
-    }
-
-    if(filename != NULL) {
-        auto gjs_cx = static_cast<GjsContext *>(JS_GetContextPrivate(context));
-        gjs_register_module(gjs_cx, filename, module, true);
-    }
-
-    JS::ModuleDeclarationInstantiation(context, module);
-    if (!JS::ModuleEvaluation(context, module))
+    if (!JS::Evaluate(context, scope_chain, options, buf, retval))
         return false;
 
     gjs_schedule_gc_if_needed(context);
