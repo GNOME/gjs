61198b5068e038c2d8aba9b756204f8e51bee158
Claudio André <claudioandre.br@gmail.com>
2018-07-01 22:36:34 -0300

    CI: move the Docker images creation to GitLab

--- test/extra/do_environment.sh
+++ test/extra/do_environment.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
 function do_Get_JHBuild(){
-    echo
-    echo '-- Download JHBuild --'
+    do_Print_Labels 'Download JHBuild'
 
     if [[ -d /jhbuild ]]; then
         # For a clean build, update and rebuild jhbuild. And avoid git pull.
@@ -18,8 +17,7 @@ function do_Get_JHBuild(){
 }
 
 function do_Configure_JHBuild(){
-    echo
-    echo '-- Set JHBuild Configuration --'
+    do_Print_Labels 'Set JHBuild Configuration'
 
     mkdir -p ~/.config
 
@@ -32,8 +30,7 @@ EOFILE
 }
 
 function do_Configure_MainBuild(){
-    echo
-    echo '-- Set Main JHBuild Configuration --'
+    do_Print_Labels 'Set Main JHBuild Configuration'
 
     mkdir -p ~/.config
     autogenargs="--enable-compile-warnings=yes --with-xvfb-tests"
@@ -54,9 +51,16 @@ EOFILE
     echo '-- Done --'
 }
 
+function do_Build_Package_Dependencies(){
+    do_Print_Labels "Building Dependencies for $1"
+    jhbuild list "$1"
+
+    # Build package dependencies
+    jhbuild build $(jhbuild list "$1" | sed '$d')
+}
+
 function do_Build_JHBuild(){
-    echo
-    echo '-- Building JHBuild --'
+    do_Print_Labels 'Building JHBuild'
 
     # Build JHBuild
     cd /jhbuild
@@ -72,6 +76,28 @@ function do_Build_JHBuild(){
     fi
     echo '-- Done --'
     cd -
+}
+
+function do_Print_Labels(){
+
+    if [[ -n "${1}" ]]; then
+        label_len=${#1}
+        span=$(((54 - $label_len) / 2))
+
+        echo
+        echo "= ======================================================== ="
+        printf "%s %${span}s %s %${span}s %s\n" "=" "" "$1" "" "="
+        echo "= ======================================================== ="
+    else
+        echo "= ========================= Done ========================= ="
+        echo
+    fi
+}
+
+function do_Done(){
+
+    # Done. De-initializes whatever is needed
+    do_Print_Labels  'FINISHED'
 }
 
 function do_Show_Info(){
