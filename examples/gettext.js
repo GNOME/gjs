386c7a4bc6e757135c03995f36df47359c51220d
Claudio André <claudioandre.br@gmail.com>
2018-07-09 20:19:25 -0300

    CI: run some real programs during tests

--- examples/gettext.js
+++ examples/gettext.js
@@ -2,13 +2,21 @@ imports.gi.versions.Gtk = '3.0';
 const Gettext = imports.gettext;
 const Gtk = imports.gi.Gtk;
 
-Gettext.bindtextdomain("gnome-panel-3.0", "/usr/share/locale");
-Gettext.textdomain("gnome-panel-3.0");
+Gettext.bindtextdomain('gnome-panel-3.0', "/usr/share/locale");
+Gettext.textdomain('gnome-panel-3.0');
 
 Gtk.init(null);
 
 let w = new Gtk.Window({ type: Gtk.WindowType.TOPLEVEL });
-w.add(new Gtk.Label({ label: Gettext.gettext("Panel") }));
+w.add(new Gtk.Label({ label: Gettext.gettext('Panel') }));
 w.show_all();
+
+// When run by automated testing
+if (ARGV.length > 0 && ARGV[0] == '_AUTO_QUIT') {
+    this.forceQuit = function() {
+        Gtk.main_quit();
+    };
+    imports.gi.GLib.timeout_add(0, 5000, this.forceQuit);
+}
 
 Gtk.main();
--- examples/webkit.js
+++ examples/webkit.js
@@ -8,11 +8,22 @@ Gtk.init(null);
 let win = new Gtk.Window();
 
 let view = new WebKit.WebView();
-view.load_uri("http://www.google.com/");
+view.load_uri('http://www.google.com/');
 win.add(view);
+
+win.connect('destroy', () => {
+    Gtk.main_quit();
+});
 
 win.set_size_request(640, 480);
 win.show_all();
 
+// When run by automated testing
+if (ARGV.length > 0 && ARGV[0] == '_AUTO_QUIT') {
+    this.forceQuit = function() {
+        Gtk.main_quit();
+    };
+    imports.gi.GLib.timeout_add(0, 10000, this.forceQuit);
+}
 Gtk.main();
 
