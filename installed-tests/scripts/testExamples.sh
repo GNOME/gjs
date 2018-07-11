94ac2b094d953f51b247888a0aa77b4f5f40759f
Claudio André <claudioandre.br@gmail.com>
2018-07-11 17:03:37 -0300

    CI: run an extra check for the installed '.sh' tests

--- installed-tests/scripts/testExamples.sh
+++ installed-tests/scripts/testExamples.sh
@@ -1,44 +1,13 @@
 #!/bin/sh
-
-if test "$GJS_USE_UNINSTALLED_FILES" = "1"; then
-    gjs="$LOG_COMPILER $LOG_FLAGS $TOP_BUILDDIR/gjs-console"
-else
-    gjs="$LOG_COMPILER $LOG_FLAGS gjs-console"
-fi
-
-# Avoid interference in the profiler tests from stray environment variable
-unset GJS_ENABLE_PROFILER
-
-total=0
-
-report () {
-    exit_code=$?
-    total=$((total + 1))
-    if test $exit_code -eq 0; then
-        echo "ok $total - $1"
-    else
-        echo "not ok $total - $1"
-    fi
-}
-
-report_xfail () {
-    exit_code=$?
-    total=$((total + 1))
-    if test $exit_code -ne 0; then
-        echo "ok $total - $1"
-    else
-        echo "not ok $total - $1"
-    fi
-}
-
-skip () {
-    total=$((total + 1))
-    echo "ok $total - $1 # SKIP $2"
-}
+DIR="$( cd "$( dirname "${0}" )" && pwd )"
+source "${DIR}"/common.sh
 
 # Run the examples
 $gjs examples/gio-cat.js Makefile
 report "run the gio-cat.js example"
+
+$gjs examples/gtk.js _AUTO_QUIT  #remove
+report "run the example gtk.js"
 
 if [[ -n "${ENABLE_GTK}" ]]; then
     $gjs examples/calc.js _AUTO_QUIT
--- test/test-ci.sh
+++ test/test-ci.sh
@@ -60,7 +60,7 @@ function do_Compare_With_Upstream_Master(){
         echo '-----------------------------------------'
         diff -u0 /cwd/master-report.txt /cwd/current-report.txt || true
         echo '-----------------------------------------'
-        exit 3
+        exit 1
     else
         echo "$REMOVED_WARNINGS warning(s) were fixed."
         echo "=> $1 Ok"
@@ -107,7 +107,20 @@ function do_Check_Warnings(){
         echo '-----------------------------------------'
         cat warnings.log || true
         echo '-----------------------------------------'
-        exit 4
+        exit 1
+    fi
+}
+
+function do_Check_Script_Erros(){
+
+    local total=0
+    total=$(cat scripts.log | grep 'not ok ' | awk '{total+=1}END{print total}')
+
+    if [[ $total > 0 ]]; then
+        echo '-----------------------------------------'
+        echo "### Found $total erros on scripts.log ###"
+        echo '-----------------------------------------'
+        exit 1
     fi
 }
 
@@ -195,6 +208,20 @@ elif [[ $1 == "VALGRIND" ]]; then
     do_Set_Env
 
     make check-valgrind
+
+elif [[ $1 == "SH_CHECKS" ]]; then
+    # It doesn't (re)build, just run the 'Tests'
+    do_Print_Labels 'Script ".sh" Check'
+    do_Set_Env
+
+    export LC_ALL=en_US.UTF-8
+    export LANG=en_US.UTF-8
+    export LANGUAGE=en_US.UTF-8
+
+    installed-tests/scripts/testCommandLine.sh > scripts.log
+    installed-tests/scripts/testExamples.sh   >> scripts.log
+    installed-tests/scripts/testWarnings.sh   >> scripts.log
+    do_Check_Script_Erros
 
 elif [[ $1 == "GJS_COVERAGE" ]]; then
     # It doesn't (re)build, just run the 'Coverage Tests'
