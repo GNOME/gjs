3f1d49bf51496837241763caab22e160c35a3c76
Claudio André <claudioandre.br@gmail.com>
2018-07-21 22:35:19 -0300

    maint: fix example eslint errors
    Run `eslint --fix examples --format unix`. Examples don't have a real
    history, so that is acceptable.

--- examples/gettext.js
+++ examples/gettext.js
@@ -2,13 +2,13 @@ imports.gi.versions.Gtk = '3.0';
 const Gettext = imports.gettext;
 const Gtk = imports.gi.Gtk;
 
-Gettext.bindtextdomain("gnome-panel-3.0", "/usr/share/locale");
-Gettext.textdomain("gnome-panel-3.0");
+Gettext.bindtextdomain('gnome-panel-3.0', '/usr/share/locale');
+Gettext.textdomain('gnome-panel-3.0');
 
 Gtk.init(null);
 
-let w = new Gtk.Window({ type: Gtk.WindowType.TOPLEVEL });
-w.add(new Gtk.Label({ label: Gettext.gettext("Panel") }));
+let w = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+w.add(new Gtk.Label({label: Gettext.gettext('Panel')}));
 w.show_all();
 
 Gtk.main();
