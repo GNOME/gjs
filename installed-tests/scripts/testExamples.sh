3a688bca1fbc52f654c466872d78d3114e3ba223
Claudio André <claudioandre.br@gmail.com>
2018-07-22 18:39:02 -0300

    CI: run the GTK real tests (recently added)
    They were disabled.

--- installed-tests/scripts/testExamples.sh
+++ installed-tests/scripts/testExamples.sh
@@ -7,21 +7,19 @@ $gjs examples/gio-cat.js Makefile
 report "run the gio-cat.js example"
 
 if [[ -n "${ENABLE_GTK}" ]]; then
-    timeout 5s $gjs examples/calc.js
+    export graphical_gjs="xvfb-run -a --server-args='-screen 0 1024x768x24' dbus-run-session -- $gjs"
+
+    eval timeout 5s $graphical_gjs examples/calc.js
     report_timeout "run the calc.js example"
 
-    timeout 5s $gjs examples/gtk.js
+    eval timeout 5s $graphical_gjs examples/gtk.js
     report_timeout "run the gtk.js example"
 
-    timeout 5s $gjs examples/gtk-application.js
-    report_timeout "run the gtk-application.js example"
-
-    timeout 5s $gjs examples/gettext.js
+    eval timeout 5s $graphical_gjs examples/gettext.js
     report_timeout "run the gettext.js example"
 else
     skip "run the calc.js example" "running without GTK"
     skip "run the gtk.js example" "running without GTK"
-    skip "run the gtk-application.js example" "running without GTK"
     skip "run the gettext.js example" "running without GTK"
 fi
 echo "1..$total"
