358a979769d14089993710b0bfbabed409a2ea6b
Philip Chimento <philip.chimento@gmail.com>
2018-08-20 22:28:52 -0400

    tests: Don't time-limit the unit tests
    Previously there was code that would intentionally crash if more than 7
    minutes elapsed, in case the tests got stuck on autobuilders. This code
    is more harmful than helpful, see the following table:
    
      Times it has killed a stuck test on GitLab: . . . . . . . . 0
      Times it has killed a JS_GC_ZEAL test:  . . . . . . . . .  34
      Times it has killed my GDB session because I forgot
        to add the stupid environment variable: . . . . . . . . 478

--- test/gjs-test-utils.h
+++ test/gjs-test-utils.h
@@ -46,8 +46,6 @@ void gjs_unit_test_fixture_teardown(GjsUnitTestFixture *fx,
 
 char *gjs_unit_test_exception_message(GjsUnitTestFixture *fx);
 
-void gjs_crash_after_timeout(int seconds);
-
 void gjs_test_add_tests_for_coverage ();
 
 void gjs_test_add_tests_for_parse_call_args(void);
