e739a46d9343b4acb625ecfb8803e6b19abbb2c5
Philip Chimento <philip.chimento@gmail.com>
2018-09-03 17:21:41 -0400

    context: Remove __gjsKeepAlive atom
    This is not used anywhere.

--- gjs/context.cpp
+++ gjs/context.cpp
@@ -88,7 +88,7 @@ struct _GjsContext {
 static const char *const_strings[] = {
     "constructor", "prototype", "length",
     "imports", "__parentModule__", "__init__", "searchPath",
-    "__gjsKeepAlive", "__gjsPrivateNS",
+    "__gjsPrivateNS",
     "gi", "versions", "overrides",
     "_init", "_instance_init", "_new_internal", "new",
     "message", "code", "stack", "fileName", "lineNumber", "columnNumber",
