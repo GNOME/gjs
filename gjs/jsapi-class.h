8529d4e8dc61a5cb7a268ddb42812bb8c6b8d064
Philip Chimento <philip.chimento@gmail.com>
2018-09-05 10:23:58 -0400

    atoms: Use more atoms to avoid converting strings
    There are a number of places where we used JS_{Get,Set,Define}Property()
    or JS_DefineFunction() with a string literal. In these cases it would be
    better to use an atom so that the string doesn't have to be converted from
    UTF-8 internally each time.
    
    In some cases there was already an atom defined, and in others we add a
    new atom.

--- gjs/jsapi-class.h
+++ gjs/jsapi-class.h
@@ -24,6 +24,7 @@
 #ifndef GJS_JSAPI_CLASS_H
 #define GJS_JSAPI_CLASS_H
 
+#include "gjs/context-private.h"
 #include "global.h"
 #include "jsapi-util.h"
 #include "jsapi-wrapper.h"
@@ -225,7 +226,7 @@ gjs_##cname##_get_proto(JSContext *cx)                                       \
     return &v_proto.toObject();                                              \
 }
 
-#define _GJS_DEFINE_DEFINE_PROTO(cname, parent_cname, ctor, gtype)           \
+#define _GJS_DEFINE_DEFINE_PROTO(cname, parent_cname, ctor, type)            \
 bool                                                                         \
 gjs_##cname##_define_proto(JSContext              *cx,                       \
                            JS::HandleObject        module,                   \
@@ -276,11 +277,12 @@ gjs_##cname##_define_proto(JSContext              *cx,                       \
     }                                                                        \
                                                                              \
     /* Define the GType value as a "$gtype" property on the constructor */   \
-    if (gtype != G_TYPE_NONE) {                                              \
+    if (type != G_TYPE_NONE) {                                               \
+        const GjsAtoms& atoms = GjsContextPrivate::atoms(cx);                \
         JS::RootedObject gtype_obj(cx,                                       \
-            gjs_gtype_create_gtype_wrapper(cx, gtype));                      \
-        if (!JS_DefineProperty(cx, ctor_obj, "$gtype", gtype_obj,            \
-                               JSPROP_PERMANENT))                            \
+            gjs_gtype_create_gtype_wrapper(cx, type));                       \
+        if (!JS_DefinePropertyById(cx, ctor_obj, atoms.gtype(), gtype_obj,   \
+                                   JSPROP_PERMANENT))                        \
             return false;                                                    \
     }                                                                        \
     gjs_debug(GJS_DEBUG_CONTEXT, "Initialized class %s prototype %p",        \
