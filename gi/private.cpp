4e8c921183ae42ce6c999794bd71569ccbb05695
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-10-09 05:52:42 +0200

    object, private: use automatic type conversion on pspec

--- gi/private.cpp
+++ gi/private.cpp
@@ -79,7 +79,7 @@ static bool gjs_override_property(JSContext* cx, unsigned argc, JS::Value* vp) {
     g_param_spec_set_qdata(new_pspec, ObjectBase::custom_property_quark(),
                            GINT_TO_POINTER(1));
 
-    args.rval().setObject(*gjs_param_from_g_param(cx, new_pspec.get()));
+    args.rval().setObject(*gjs_param_from_g_param(cx, new_pspec));
 
     return true;
 }
