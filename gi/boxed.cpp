f81d397551d80ba5107d245a2fb576caa3aaeb48
Philip Chimento <philip.chimento@gmail.com>
2018-11-23 22:14:28 -0500

    repo: Factor out new function
    The combination of gjs_lookup_generic_prototype() plus
    JS_NewObjectWithGivenProto() occurs often enough to be a separate
    function.

--- gi/boxed.cpp
+++ gi/boxed.cpp
@@ -447,17 +447,10 @@ bool BoxedInstance::get_nested_interface_object(
         return false;
     }
 
-    JS::RootedObject proto(
-        context, gjs_lookup_generic_prototype(context, interface_info));
-
-    if (!proto)
-        return false;
-
     offset = g_field_info_get_offset (field_info);
 
-    JS::RootedObject obj(context, JS_NewObjectWithGivenProto(
-                                      context, JS_GetClass(proto), proto));
-
+    JS::RootedObject obj(context, gjs_new_object_with_generic_prototype(
+                                      context, interface_info));
     if (!obj)
         return false;
 
@@ -963,12 +956,7 @@ JSObject* gjs_boxed_from_c_struct(JSContext* cx, GIStructInfo* info,
                       "Wrapping struct %s %p with JSObject",
                       g_base_info_get_name((GIBaseInfo *)info), gboxed);
 
-    JS::RootedObject proto(cx, gjs_lookup_generic_prototype(cx, info));
-    if (!proto)
-        return nullptr;
-
-    JS::RootedObject obj(
-        cx, JS_NewObjectWithGivenProto(cx, JS_GetClass(proto), proto));
+    JS::RootedObject obj(cx, gjs_new_object_with_generic_prototype(cx, info));
     if (!obj)
         return nullptr;
 
