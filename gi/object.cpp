f960433ddc1d16709e01fca9e42a7f3099821e6e
Philip Chimento <philip.chimento@gmail.com>
2019-01-22 22:38:07 -0800

    object: Port object_init_list to GCVector
    Previously we used a std::stack with individual instances of
    JS::PersistentRootedObject in it, to hold a list of JSObjects in between
    the time of their creation and the time their GObject instance init
    function runs. This is wasteful because each individual PersistentRooted
    is part of a linked list of permanently rooted objects. Instead, we can
    put them in a GCVector and persistently root the whole thing once.
    
    We also move that storage from a static variable to a member of the
    GjsContext class.

--- gi/object.cpp
+++ gi/object.cpp
@@ -24,12 +24,6 @@
 
 #include <config.h>
 
-#include <memory>
-#include <string.h>
-#include <tuple>
-#include <unordered_map>
-#include <vector>
-
 #include <mozilla/Unused.h>
 
 #include "gi/arg.h"
@@ -71,7 +65,6 @@ static_assert(sizeof(ObjectInstance) <= 88,
               "gnome-shell run.");
 #endif  // x86-64 clang
 
-std::stack<JS::PersistentRootedObject> ObjectInstance::object_init_list{};
 bool ObjectInstance::s_weak_pointer_callback = false;
 ObjectInstance *ObjectInstance::wrapped_gobject_list = nullptr;
 
@@ -1471,8 +1464,13 @@ ObjectInstance::init_impl(JSContext              *context,
        will be popped in gjs_object_custom_init() later
        down.
     */
-    if (g_type_get_qdata(gtype(), ObjectInstance::custom_type_quark()))
-        object_init_list.emplace(context, object);
+    if (g_type_get_qdata(gtype(), ObjectInstance::custom_type_quark())) {
+        GjsContextPrivate* gjs = GjsContextPrivate::from_cx(context);
+        if (!gjs->object_init_list().append(object)) {
+            JS_ReportOutOfMemory(context);
+            return false;
+        }
+    }
 
     g_assert(names.size() == values.size());
     GObject* gobj = g_object_new_with_properties(gtype(), values.size(),
