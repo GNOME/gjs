e3e2bc0c2b6c2f9e6c541e6acb7c19a17f3835d7
Philip Chimento <philip.chimento@gmail.com>
2019-03-09 21:10:04 -0800

    Gio: Add non-crashing overrides for Gio.Settings
    Gio.Settings aborts intentionally if it is asked to load a nonexistent
    schema or access a nonexistent key. This is all well and good for C
    programs that install their own schemas, but a problem that many GNOME
    Shell extensions authors end up running into when trying to deal with
    multiple GNOME versions at once.
    
    Based on code from Cinnamon written by Michael Webster. See
    https://github.com/linuxmint/Cinnamon/blob/master/js/ui/overrides.js#L21-L98
    Used with permission of the copyright holder, see comments in #205.
    
    Closes: #205

--- modules/overrides/Gio.js
+++ modules/overrides/Gio.js
@@ -490,4 +490,98 @@ function _init() {
 
     // Temporary Gio.File.prototype fix
     Gio._LocalFilePrototype = Gio.File.new_for_path('').constructor.prototype;
+
+    // Override Gio.Settings and Gio.SettingsSchema - the C API asserts if
+    // trying to access a nonexistent schema or key, which is not handy for
+    // shell-extension writers
+
+    Gio.SettingsSchema.prototype._realGetKey = Gio.SettingsSchema.prototype.get_key;
+    Gio.SettingsSchema.prototype.get_key = function(key) {
+        if (!this.has_key(key))
+            throw new Error(`GSettings key ${key} not found in schema ${this.get_id()}`);
+        return this._realGetKey(key);
+    };
+
+    Gio.Settings.prototype._realMethods = Object.assign({}, Gio.Settings.prototype);
+
+    function createCheckedMethod(method, checkMethod = '_checkKey') {
+        return function(id, ...args) {
+            this[checkMethod](id);
+            return this._realMethods[method].call(this, id, ...args);
+        };
+    }
+
+    Object.assign(Gio.Settings.prototype, {
+        _realInit: Gio.Settings.prototype._init,  // add manually, not enumerable
+        _init(props = {}) {
+            // 'schema' is a deprecated alias for schema_id
+            const requiredProps = ['schema', 'schema-id', 'schema_id', 'schemaId',
+                'settings-schema', 'settings_schema', 'settingsSchema'];
+            if (requiredProps.every(prop => !(prop in props)))
+                throw new Error("One of property 'schema-id' or " +
+                    "'settings-schema' are required for Gio.Settings");
+
+            const checkSchemasProps = ['schema', 'schema-id', 'schema_id', 'schemaId'];
+            const source = Gio.SettingsSchemaSource.get_default();
+            for (const prop of checkSchemasProps) {
+                if (!(prop in props))
+                    continue;
+                if (source.lookup(props[prop], true) === null)
+                    throw new Error(`GSettings schema ${props[prop]} not found`);
+            }
+
+            return this._realInit(props);
+        },
+
+        _checkKey(key) {
+            // Avoid using has_key(); checking a JS array is faster than calling
+            // through G-I.
+            if (!this._keys)
+                this._keys = this.settings_schema.list_keys();
+
+            if (!this._keys.includes(key))
+                throw new Error(`GSettings key ${key} not found in schema ${this.schema_id}`);
+        },
+
+        _checkChild(name) {
+            if (!this._children)
+                this._children = this.list_children();
+
+            if (!this._children.includes(name))
+                throw new Error(`Child ${name} not found in GSettings schema ${this.schema_id}`);
+        },
+
+        get_boolean: createCheckedMethod('get_boolean'),
+        set_boolean: createCheckedMethod('set_boolean'),
+        get_double: createCheckedMethod('get_double'),
+        set_double: createCheckedMethod('set_double'),
+        get_enum: createCheckedMethod('get_enum'),
+        set_enum: createCheckedMethod('set_enum'),
+        get_flags: createCheckedMethod('get_flags'),
+        set_flags: createCheckedMethod('set_flags'),
+        get_int: createCheckedMethod('get_int'),
+        set_int: createCheckedMethod('set_int'),
+        get_int64: createCheckedMethod('get_int64'),
+        set_int64: createCheckedMethod('set_int64'),
+        get_string: createCheckedMethod('get_string'),
+        set_string: createCheckedMethod('set_string'),
+        get_strv: createCheckedMethod('get_strv'),
+        set_strv: createCheckedMethod('set_strv'),
+        get_uint: createCheckedMethod('get_uint'),
+        set_uint: createCheckedMethod('set_uint'),
+        get_uint64: createCheckedMethod('get_uint64'),
+        set_uint64: createCheckedMethod('set_uint64'),
+        get_value: createCheckedMethod('get_value'),
+        set_value: createCheckedMethod('set_value'),
+
+        bind: createCheckedMethod('bind'),
+        bind_writable: createCheckedMethod('bind_writable'),
+        create_action: createCheckedMethod('create_action'),
+        get_default_value: createCheckedMethod('get_default_value'),
+        get_user_value: createCheckedMethod('get_user_value'),
+        is_writable: createCheckedMethod('is_writable'),
+        reset: createCheckedMethod('reset'),
+
+        get_child: createCheckedMethod('get_child', '_checkChild'),
+    });
 }
