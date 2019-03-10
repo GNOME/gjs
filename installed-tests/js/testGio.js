19c7d155afe6eefe1cacce9476d1cbc987b3dd0f
Philip Chimento <philip.chimento@gmail.com>
2019-03-09 21:10:04 -0800

    Gio: Add non-crashing overrides for Gio.Settings
    Gio.Settings aborts intentionally if it is asked to load a nonexistent
    schema or access a nonexistent key. This is all well and good for C
    programs that install their own schemas, but a problem that many GNOME
    Shell extensions authors end up running into when trying to deal with
    multiple GNOME versions at once.
    
    Based on code from Cinnamon written by Michael Webster. See
    https://github.com/linuxmint/Cinnamon/blob/master/js/ui/overrides.js#L21-L98
    Used with permission of the copyright holder, see comments in #205.
    
    Closes: #205

--- installed-tests/js/testGio.js
+++ installed-tests/js/testGio.js
@@ -1,5 +1,4 @@
-const Gio = imports.gi.Gio;
-const GObject = imports.gi.GObject;
+const {GLib, Gio, GObject} = imports.gi;
 
 const Foo = GObject.registerClass(class Foo extends GObject.Object {
     _init(value) {
@@ -23,5 +22,60 @@ describe('ListStore iterator', function () {
         for (let f of list) {
             expect(f.value).toBe(i++);
         }
+    });
+});
+
+describe('Gio.Settings overrides', function () {
+    it("throws but doesn't crash when forgetting to specify a schema ID", function () {
+        expect(() => new Gio.Settings()).toThrowError(/schema/);
+    });
+
+    it("throws but doesn't crash when specifying a schema ID that isn't installed", function () {
+        expect(() => new Gio.Settings({schema: 'com.example.ThisDoesntExist'}))
+            .toThrowError(/schema/);
+    });
+
+    describe('with existing schema', function () {
+        const KINDS = ['boolean', 'double', 'enum', 'flags', 'int', 'int64',
+            'string', 'strv', 'uint', 'uint64', 'value'];
+        let settings;
+
+        beforeEach(function () {
+            settings = new Gio.Settings({schema: 'org.gnome.GjsTest'});
+        });
+
+        it("throws but doesn't crash when resetting a key that doesn't belong to the schema", function () {
+            expect(() => settings.reset('foobar')).toThrowError(/key/);
+        });
+
+        it("throws but doesn't crash when checking a key that doesn't belong to the schema", function () {
+            KINDS.forEach(kind => {
+                expect(() => settings[`get_${kind}`]('foobar')).toThrowError(/key/);
+            });
+        });
+
+        it("throws but doesn't crash when setting a key that doesn't belong to the schema", function () {
+            KINDS.forEach(kind => {
+                expect(() => settings[`set_${kind}`]('foobar', null)).toThrowError(/key/);
+            });
+        });
+
+        it('still works with correct keys', function () {
+            expect(() => {
+                settings.set_value('window-size', new GLib.Variant('(ii)', [100, 100]));
+                settings.set_boolean('maximized', true);
+                settings.set_boolean('fullscreen', true);
+            }).not.toThrow();
+
+            expect(settings.get_value('window-size').deep_unpack()).toEqual([100, 100]);
+            expect(settings.get_boolean('maximized')).toEqual(true);
+            expect(settings.get_boolean('fullscreen')).toEqual(true);
+
+            expect(() => {
+                settings.reset('window-size');
+                settings.reset('maximized');
+                settings.reset('fullscreen');
+            }).not.toThrow();
+        });
     });
 });
