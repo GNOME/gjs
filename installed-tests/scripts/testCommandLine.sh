6fa6d196d8bc2e6e452a86cf14563b21ef10d399
Philip Chimento <philip.chimento@gmail.com>
2019-04-13 11:42:50 -0700

    console: Don't accept --profile after the script name
    This was only intended for arguments that were previously accepted after
    the script name on the command line. For example, in some previous
    versions,
    
        gjs myapp.js -C myfiles
    
    would pass the -C argument to GJS and hide it from myapp.js. We changed
    this some time ago, but kept the behaviour for these arguments while
    logging a warning.
    
    However, new arguments should not get this behaviour. I accidentally
    implemented it for --profile. Technically it's not backwards compatible
    to remove it now, but I think it's OK because nobody would ever have
    been able to write
    
        gjs myapp.js --profile myapp.syscap
    
    without getting a warning stating that they shouldn't do that.

--- installed-tests/scripts/testCommandLine.sh
+++ installed-tests/scripts/testCommandLine.sh
@@ -155,9 +155,6 @@ report "--coverage-prefix after script should succeed but give a warning"
 $gjs -c 'imports.system.exit(0)' --coverage-prefix=foo --coverage-output=foo 2>&1 | grep -q 'Gjs-WARNING.*--coverage-output'
 report "--coverage-output after script should succeed but give a warning"
 rm -f foo/coverage.lcov
-$gjs -c 'imports.system.exit(0)' --profile=foo 2>&1 | grep -q 'Gjs-WARNING.*--profile'
-report "--profile after script should succeed but give a warning"
-rm -rf foo
 
 for version_arg in --version --jsversion; do
     # --version and --jsversion work
