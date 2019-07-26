ea7a74350a4a703d1f3658d4e133bcf710997a31
Evan Welsh <noreply@evanwelsh.com>
2019-07-26 06:58:06 -0400

    Add ESM shims for existing system modules.

---
+++ modules/esm/byteArray.js
@@ -0,0 +1,13 @@
+
+/**
+ * @constructor
+ */
+export const ByteArray = imports.byteArray.ByteArray;
+
+/**
+ * @param {*} a
+ * @returns {ByteArray} 
+ */
+export function fromArray(a) {
+    return imports.byteArray.fromArray(a);
+}
