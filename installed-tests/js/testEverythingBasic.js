62b65f69c1b14002bdf91c17e4ab4f4b10d2d699
Philip Chimento <philip.chimento@gmail.com>
2019-08-04 20:56:55 -0700

    tests: Reintroduce skipped tests that have been fixed
    Many of these skipped tests were in fact waiting on GObject
    introspection testing API, which was added in 1.58.0. Some others have
    just been fixed along the way.

--- installed-tests/js/testEverythingBasic.js
+++ installed-tests/js/testEverythingBasic.js
@@ -417,12 +417,12 @@ describe('Life, the Universe and Everything', function () {
         expect(Regress.TestEnum.param(Regress.TestEnum.VALUE4)).toEqual('value4');
     });
 
-    xit('can be answered with GObject.set()', function() {
+    it('can be answered with GObject.set()', function() {
         let o = new Regress.TestObj();
         o.set({ string: 'Answer', int: 42 });
         expect(o.string).toBe('Answer');
         expect(o.int).toBe(42);
-    }).pend('https://gitlab.gnome.org/GNOME/gobject-introspection/issues/113');
+    });
 
     describe('Object properties on GtkBuilder-constructed objects', function () {
         let o1;
@@ -484,7 +484,7 @@ describe('Life, the Universe and Everything', function () {
         });
     });
 
-    xdescribe('Overridden properties on interfaces', function () {
+    describe('Overridden properties on interfaces', function () {
         it('set and get properly', function () {
             const o = new Regress.TestSubObj();
             o.number = 4;
@@ -501,7 +501,7 @@ describe('Life, the Universe and Everything', function () {
             const o = new Regress.TestSubObj({number: 4});
             expect(o.number).toEqual(4);
         });
-    }).pend('https://gitlab.gnome.org/GNOME/gobject-introspection/merge_requests/59');
+    });
 
     describe('Signal connection', function () {
         let o;
