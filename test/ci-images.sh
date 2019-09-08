a7391c4f6f2cd531d6f2b7c3e739875a4f15c35e
Philip Chimento <philip.chimento@gmail.com>
2019-09-07 22:53:25 -0700

    CI: Consolidate image build scripts in test/ci-images.sh
    The scripts are a lot smaller now that we build fewer images and don't
    use jhbuild, so it's small enough to keep in one file. That makes it
    easier to read.

--- test/ci-images.sh
+++ test/ci-images.sh
@@ -1,5 +1,29 @@
 #!/bin/bash -e
 
+function do_Install_Dependencies(){
+    echo
+    echo '-- Installing Base Dependencies --'
+
+    dnf -y upgrade --best --allowerasing
+
+    # Base dependencies
+    dnf -y    install @c-development @development-tools clang redhat-rpm-config gnome-common python-devel \
+                      pygobject2 dbus-python perl-Text-CSV perl-XML-Parser gettext-devel gtk-doc \
+                      autoconf-archive meson ninja-build zlib-devel libffi-devel \
+                      libtool libicu-devel nspr-devel systemtap-sdt-devel \
+                      gtk3 gtk3-devel gobject-introspection-devel Xvfb gnome-desktop-testing dbus-x11 \
+                      cairo intltool libxslt bison nspr python3-devel dbus-glib libicu \
+                      libxslt libtool flex \
+                      cairo-devel zlib-devel libffi-devel pcre-devel libxml2-devel libxslt-devel \
+                      libedit-devel libasan libubsan libtsan compiler-rt \
+                      sysprof-devel lcov mesa-libGL-devel readline-devel \
+                      webkit2gtk3 time
+
+    # Distros debug info of needed libraries
+    dnf -y debuginfo-install glib2-devel gobject-introspection-devel \
+      gtk3-devel expat fontconfig cairo glibc
+}
+
 function do_Set_Env(){
     echo
     echo '-- Set Environment --'
@@ -14,6 +38,35 @@ function do_Set_Env(){
     if [[ -z "${DISPLAY}" ]]; then
         export DISPLAY=":0"
     fi
+
+    echo '-- Done --'
+}
+
+function do_Build_Mozilla(){
+    echo
+    echo '-- Building Mozilla SpiderMonkey --'
+
+    git clone --depth 1 https://github.com/ptomato/mozjs.git -b "${MOZJS_BRANCH:-mozjs60}" /on-host/mozjs
+    cd /on-host/mozjs
+
+    mkdir -p _build
+    cd _build
+
+    ../js/src/configure --prefix=/root/jhbuild/install --enable-posix-nspr-emulation --with-system-zlib --with-intl-api --disable-jemalloc AUTOCONF=autoconf ${BUILD_OPTS}
+    make -sj4
+    make install
+
+    cd -
+}
+
+function do_Shrink_Image(){
+    echo
+    echo '-- Cleaning image --'
+    PATH=$PATH:~/.local/bin
+    rm -rf ~/jhbuild/install/lib/libjs_static.ajs
+
+    dnf -y clean all
+    rm -rf /var/cache/dnf
 
     echo '-- Done --'
 }
@@ -36,9 +89,15 @@ do_Print_Labels  'ENVIRONMENT'
 echo "Running on: $OS"
 echo "Doing: $1 $extra_opts"
 
-source test/extra/do_basic.sh
-source test/extra/do_mozilla.sh
-source test/extra/do_docker.sh
+if [[ $1 == "BUILD_MOZ" ]]; then
+    do_Install_Dependencies
+    do_Set_Env
+    do_Show_Info
+    do_Build_Mozilla
+    do_Shrink_Image
+fi
+# Clear the environment
+unset BUILD_OPTS
 
 # Releases stuff and finishes
 do_Done
