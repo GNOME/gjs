5056257488e42790334f2018daacb9ec84465a80
Chun-wei Fan <fanchunwei@src.gnome.org>
2019-10-25 17:41:02 +0800

    meson: Convert build/compile-gschemas.sh into Python
    This is so that we can also use this script on Windows MSVC builds,
    which do not support shell scripts out-of-the-box.

--- build/compile-gschemas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SCHEMADIR="$MESON_INSTALL_PREFIX/share/glib-2.0/schemas"
-
-if test -z "$DESTDIR"; then
-    echo "Compiling GSettings schemas..."
-    glib-compile-schemas "$SCHEMADIR"
-fi
