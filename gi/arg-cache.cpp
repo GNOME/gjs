GNOME/gjs

commit 1f3984c9257560603acc0be0c44fd68f1ebbfb16

arg-cache: extend to handle interface types too
Handle enums, objects and boxed types through the argument cache
rather than using the generic marshaller.

(Philip Chimento: rebased and fixed coding style and bugs, and added
stubs for GParamSpec and GdkAtom.)

gi/arg-cache.h
@@ -27,8 +27,10 @@
 #include <config.h>
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include <girepository.h>
+#include <glib-object.h>
 
 #include <js/RootingAPI.h>
 #include <js/TypeDecls.h>
@@ -47,6 +49,7 @@ typedef struct _GjsArgumentCache {
     bool (*release)(JSContext* cx, struct _GjsArgumentCache* cache,
                     GjsFunctionCallState* state, GIArgument* in_argument,
                     GIArgument* out_argument);
+    void (*free)(struct _GjsArgumentCache* cache);
 
     const char* arg_name;
     int arg_pos;
@@ -75,6 +78,22 @@ typedef struct _GjsArgumentCache {
             GITypeTag number_tag;
             bool is_unsigned : 1;
         } number;
+
+        // boxed / union / GObject
+        struct {
+            GType gtype;
+            GIBaseInfo* info;
+        } object;
+
+        // foreign structures
+        GIStructInfo* tmp_foreign_info;
+
+        // enum / flags
+        struct {
+            int64_t enum_min;
+            int64_t enum_max;
+        } enum_type;
+        uint64_t flags_mask;
 
         // string / filename
         bool string_is_filename : 1;

gi/arg-inl.h
@@ -147,6 +147,13 @@ GJS_USE inline decltype(auto) gjs_arg_member<int, GI_TYPE_TAG_INTERFACE>(
     return gjs_arg_member(arg, &GIArgument::v_int);
 }
 
+// Unsigned enums
+template <>
+GJS_USE inline decltype(auto) gjs_arg_member<unsigned, GI_TYPE_TAG_INTERFACE>(
+    GIArgument* arg) {
+    return gjs_arg_member(arg, &GIArgument::v_uint);
+}
+
 template <typename T, GITypeTag TAG = GI_TYPE_TAG_VOID>
 inline std::enable_if_t<!std::is_pointer_v<T>> gjs_arg_set(GIArgument* arg,
                                                            T v) {

gi/function.cpp
@@ -1119,15 +1119,22 @@ GJS_NATIVE_CONSTRUCTOR_DEFINE_ABSTRACT(function)
 static void
 uninit_cached_function_data (Function *function)
 {
+    g_assert(function->info && "Don't know how to free cache without GI info");
+
     if (function->arguments) {
         // Careful! function->arguments is offset by one element inside
         // the allocated space, so we have to free index -1.
+        int gi_argc = g_callable_info_get_n_args(function->info);
+        for (int ix = -1; ix < gi_argc; ix++) {
+            if (function->arguments[ix].free)
+                function->arguments[ix].free(&function->arguments[ix]);
+        }
+
         g_free(&function->arguments[-1]);
         function->arguments = nullptr;
     }
 
-    if (function->info)
-        g_base_info_unref( (GIBaseInfo*) function->info);
+    g_base_info_unref(function->info);
 
     g_function_invoker_destroy(&function->invoker);
 }

gjs/jsapi-util.h
@@ -147,6 +147,7 @@ using GjsAutoObjectInfo = GjsAutoInfo<GI_INFO_TYPE_OBJECT>;
 using GjsAutoPropertyInfo = GjsAutoInfo<GI_INFO_TYPE_PROPERTY>;
 using GjsAutoStructInfo = GjsAutoInfo<GI_INFO_TYPE_STRUCT>;
 using GjsAutoTypeInfo = GjsAutoInfo<GI_INFO_TYPE_TYPE>;
+using GjsAutoValueInfo = GjsAutoInfo<GI_INFO_TYPE_VALUE>;
 using GjsAutoVFuncInfo = GjsAutoInfo<GI_INFO_TYPE_VFUNC>;
 
 // GICallableInfo can be one of several tags, so we have to have a separate

commit 1716c8667677f57f90be13aca7b7f6d90e0a5ece

arg-cache: Always use an unsigned int mask for flags
When retrieving flags from the GIArgumentInfo in
set_return_ffi_arg_from_giargument(), we use the v_int member of the
union (arguably we ought to use the v_uint member for flags, but we
don't), so to avoid undefined behaviour from type-punning we need to
store flags via that same member.

In particular, even if we somehow convince ourselves that a flags type
can have values outside the 32-bit range, it would be incorrect to
store them in the v_int64 or v_uint64 member, because when we read
them back out it'll be via the v_int member. That's formally undefined
behaviour, but in practice will access the first 32 bits of the 64-bit
integer, which is the same as (real_value & 0xffff'ffff) on
little-endian CPUs, but gives a meaningless result on big-endian.
Sure enough, after 1f3984c9 the unit tests start failing on s390x,
which is 64-bit big-endian.

GLib's GFlagsClass.mask and GFlagsValue.value are of type guint
(equivalent to unsigned int), so it can't possibly represent flags
outside the range of guint as a GType anyway. To be consistent with
that (and have reasonable unsigned arithmetic), go via an unsigned
integer.

The corresponding fix for enum types was done as a side-effect of
8b2927b0 "arg-cache: Save space in enum bounds".

Fixes: 1f3984c9 "arg-cache: extend to handle interface types too"
Resolves: https://gitlab.gnome.org/GNOME/gjs/-/issues/341
Signed-off-by: Simon McVittie <smcv@debian.org>

gi/arg-cache.cpp
@@ -572,11 +572,10 @@ static bool gjs_marshal_flags_in_in(JSContext* cx, GjsArgumentCache* self,
         return false;
     }
 
-    if (self->contents.flags_mask <= G_MAXUINT32)
-        gjs_arg_set<unsigned, GI_TYPE_TAG_INTERFACE>(arg, number);
-    else
-        gjs_arg_set(arg, uint64_t(number));
-
+    // We cast to unsigned because that's what makes sense, but then we
+    // put it in the v_int slot because that's what we use to unmarshal
+    // flags types at the moment.
+    gjs_arg_set<int, GI_TYPE_TAG_INTERFACE>(arg, static_cast<unsigned>(number));
     return true;
 }
 
@@ -1292,8 +1291,13 @@ static void gjs_arg_cache_build_flags_mask(GjsArgumentCache* self,
     int n = g_enum_info_get_n_values(enum_info);
     for (int i = 0; i < n; i++) {
         GjsAutoValueInfo value_info = g_enum_info_get_value(enum_info, i);
-        uint64_t value = uint64_t(g_value_info_get_value(value_info));
-        mask |= value;
+        int64_t value = g_value_info_get_value(value_info);
+        // From the docs for g_value_info_get_value(): "This will always be
+        // representable as a 32-bit signed or unsigned value. The use of
+        // gint64 as the return type is to allow both."
+        // We stuff both into an unsigned, int-sized field, matching the
+        // internal representation of flags in GLib (which uses guint).
+        mask |= static_cast<unsigned>(value);
     }
 
     self->contents.flags_mask = mask;

gi/arg-cache.h
@@ -93,7 +93,7 @@ struct GjsArgumentCache {
             uint32_t enum_min;
             uint32_t enum_max;
         } enum_type;
-        uint64_t flags_mask;
+        unsigned flags_mask;
 
         // string / filename
         bool string_is_filename : 1;