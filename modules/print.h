c69620ab63f574ae148fa1ff8fc25271abb7d8a2
Evan Welsh <contact@evanwelsh.com>
2020-11-23 21:05:55 -0600

    Correct contributor copyright email.
    Future commits will also be made under this email address.

--- modules/print.h
+++ modules/print.h
@@ -1,6 +1,6 @@
 /* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; -*- */
 // SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
-// SPDX-FileCopyrightText: 2020 Evan Welsh <noreply@evanwelsh.com>
+// SPDX-FileCopyrightText: 2020 Evan Welsh <contact@evanwelsh.com>
 
 #ifndef MODULES_PRINT_H_
 #define MODULES_PRINT_H_
