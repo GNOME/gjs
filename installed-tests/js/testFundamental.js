696163987e19cf4f6263538915c41256ae08cad1
Marco Trevisan (Treviño) <mail@3v1n0.net>
2021-04-08 00:12:02 +0200

    fundamental: Use value or instance pointer when no get/set funcs are provided
    Some fundamental types are providing value set/get functions however
    this is not mandatory as the fundamental can be just retrieved from the
    GValue pointer or we can build a GValue using the instance and gtype.
    
    A case for this are Gdk4 Events that does not provide such functions,
    making impossible to use events signals.
    
    Add tests simulating this, depending on a new gobject-introspection type
    that respects this constrain.
    
    Fixes: #398

--- installed-tests/js/testFundamental.js
+++ installed-tests/js/testFundamental.js
@@ -7,6 +7,9 @@ const {GObject, Regress} = imports.gi;
 const TestObj = GObject.registerClass({
     Signals: {
         'test-fundamental-value-funcs': {param_types: [Regress.TestFundamentalSubObject.$gtype]},
+        'test-fundamental-no-funcs': {param_types:
+            Regress.TestFundamentalObjectNoGetSetFunc
+                ? [Regress.TestFundamentalObjectNoGetSetFunc.$gtype] : []},
     },
 }, class TestObj extends GObject.Object {});
 
@@ -21,4 +24,27 @@ describe('Fundamental type support', function () {
         obj.emit('test-fundamental-value-funcs', fund);
         expect(signalSpy).toHaveBeenCalledWith(obj, fund);
     });
+
+    it('can marshal a custom fundamental type into a GValue if contains a pointer and does not provide setter and getters', function () {
+        const fund = new Regress.TestFundamentalObjectNoGetSetFunc('foo');
+        expect(() => GObject.strdup_value_contents(fund)).not.toThrow();
+
+        const obj = new TestObj();
+        const signalSpy = jasmine.createSpy('signalSpy');
+        obj.connect('test-fundamental-no-funcs', signalSpy);
+        obj.connect('test-fundamental-no-funcs', (_o, f) =>
+            expect(f.get_data()).toBe('foo'));
+        obj.emit('test-fundamental-no-funcs', fund);
+        expect(signalSpy).toHaveBeenCalledWith(obj, fund);
+    }).pend('https://gitlab.gnome.org/GNOME/gobject-introspection/-/merge_requests/268');
+
+    it('cannot marshal a custom fundamental type into a GValue of different gtype', function () {
+        const fund = new Regress.TestFundamentalObjectNoGetSetFunc('foo');
+
+        const obj = new TestObj();
+        const signalSpy = jasmine.createSpy('signalSpy');
+        obj.connect('test-fundamental-value-funcs', signalSpy);
+        expect(() => obj.emit('test-fundamental-value-funcs', fund)).toThrowError(
+            /conversion to a GValue/);
+    }).pend('https://gitlab.gnome.org/GNOME/gobject-introspection/-/merge_requests/268');
 });
