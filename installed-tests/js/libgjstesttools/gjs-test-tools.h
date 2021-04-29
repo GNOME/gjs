b14f93e207a4be0b149b1016f5307af842d34053
Chun-wei Fan <fanchunwei@src.gnome.org>
2021-04-29 16:07:42 +0800

    installed-tests: Fix building libgjstesttools
    ...on Visual Studio-style builds, since we need to explicitly mark the
    prototypes in the header for export, otherwise the build will fail as
    running g-ir-scanner will fail to find the export library for the DLL
    that was built.
    
    (cherry-picked from commit commit 76b092c1)

--- installed-tests/js/libgjstesttools/gjs-test-tools.h
+++ installed-tests/js/libgjstesttools/gjs-test-tools.h
@@ -4,49 +4,74 @@
 
 #pragma once
 
+#ifdef _GI_EXTERN
+#define _GJS_TEST_TOOL_EXTERN _GI_EXTERN
+#else
+#define _GJS_TEST_TOOL_EXTERN
+#endif
+
 #include <gio/gio.h>
 #include <glib-object.h>
 #include <glib.h>
 
 G_BEGIN_DECLS
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_init(void);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_reset(void);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_delayed_ref(GObject* object, int interval);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_delayed_unref(GObject* object, int interval);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_delayed_dispose(GObject* object, int interval);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_ref_other_thread(GObject* object);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_delayed_ref_other_thread(GObject* object, int interval);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_unref_other_thread(GObject* object);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_delayed_unref_other_thread(GObject* object, int interval);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_delayed_ref_unref_other_thread(GObject* object,
                                                    int interval);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_run_dispose_other_thread(GObject* object);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_save_object(GObject* object);
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_save_object_unreffed(GObject* object);
 
+_GJS_TEST_TOOL_EXTERN
 GObject* gjs_test_tools_get_saved();
 
+_GJS_TEST_TOOL_EXTERN
 GObject* gjs_test_tools_steal_saved();
 
+_GJS_TEST_TOOL_EXTERN
 void gjs_test_tools_save_weak(GObject* object);
 
+_GJS_TEST_TOOL_EXTERN
 GObject* gjs_test_tools_get_weak();
 
+_GJS_TEST_TOOL_EXTERN
 GObject* gjs_test_tools_get_weak_other_thread();
 
+_GJS_TEST_TOOL_EXTERN
 GObject* gjs_test_tools_get_disposed(GObject* object);
 
 G_END_DECLS
