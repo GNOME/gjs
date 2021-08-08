816c41beeb036528f4462ada90e3e3e599e33393
Sonny Piers <sonny@fastmail.net>
2021-08-08 15:02:04 +0000

    examples: improve the gettext example
    gnome-panel-3.0 isn't a thing anymore

--- examples/gettext.js
+++ examples/gettext.js
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
 // SPDX-FileCopyrightText: 2009 Red Hat, Inc.
 
+/*
+ * Make sure you have a non english locale installed, for example fr_FR and run
+ * LANGUAGE=fr_FR gjs gettext.js
+ * the label should show a translation of 'Print help'
+ */
+
 imports.gi.versions.Gtk = '3.0';
 const Gettext = imports.gettext;
 const Gtk = imports.gi.Gtk;
 
-Gettext.bindtextdomain('gnome-panel-3.0', '/usr/share/locale');
-Gettext.textdomain('gnome-panel-3.0');
+Gettext.bindtextdomain('gnome-shell', '/usr/share/locale');
+Gettext.textdomain('gnome-shell');
 
 Gtk.init(null);
 
 let w = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
-w.add(new Gtk.Label({label: Gettext.gettext('Panel')}));
+w.add(new Gtk.Label({label: Gettext.gettext('Print help')}));
 w.show_all();
 
 Gtk.main();
