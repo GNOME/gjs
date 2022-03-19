2c9513c734ffc1a86cb492a1ff29a3135c060e2e
Evan Miller <emmiller@gmail.com>
2022-03-18 17:58:12 -0700

    profiler: Align types in function definition with header file
    Same gint64/int64_t type mismatch on Darwin (long vs long long) as the
    previous commit.

--- gjs/profiler.cpp
+++ gjs/profiler.cpp
@@ -700,8 +700,8 @@ gjs_profiler_set_filename(GjsProfiler *self,
     self->filename = g_strdup(filename);
 }
 
-void _gjs_profiler_add_mark(GjsProfiler* self, gint64 time_nsec,
-                            gint64 duration_nsec, const char* group,
+void _gjs_profiler_add_mark(GjsProfiler* self, int64_t time_nsec,
+                            int64_t duration_nsec, const char* group,
                             const char* name, const char* message) {
     g_return_if_fail(self);
     g_return_if_fail(group);
