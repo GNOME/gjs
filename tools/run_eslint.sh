f55d0916e7b31ada7378d229a26a120728700bbc
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-11-07 08:10:54 +0100

    Configure npm to prefer IPv4 addresses

--- tools/run_eslint.sh
+++ tools/run_eslint.sh
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
 # SPDX-FileCopyrightText: 2020 Philip Chimento <philip.chimento@gmail.com>
 
+export NODE_OPTIONS=--dns-result-order=ipv4first
+
 cd $(dirname -- "$0")
 npm ci
 npm run lint -- "$@"
