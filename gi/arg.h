b4fa0e3152a0bd963fc5f24ea7f5dc88f9ee544d
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-04-10 17:26:20 +0200

    gi/arg: Use the argument type to decide the structs transfer to apply
    We only need to explicitly fully copy items if they're elements of a
    container. As in those cases we temporary own them.

--- gi/arg.h
+++ gi/arg.h
@@ -143,6 +143,7 @@ bool gjs_array_to_strv (JSContext   *context,
 
 GJS_JSAPI_RETURN_CONVENTION
 bool gjs_array_from_g_value_array(JSContext* cx, JS::MutableHandleValue value_p,
-                                  GITypeInfo* param_info, const GValue* gvalue);
+                                  GITypeInfo* param_info, GITransfer,
+                                  const GValue* gvalue);
 
 #endif  // GI_ARG_H_
