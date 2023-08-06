87c03ad858185219741b7ee7fa9a5132f64902c1
Philip Chimento <philip.chimento@gmail.com>
2023-08-06 16:42:48 +0200

    native: Fix outdated comments

--- gjs/native.h
+++ gjs/native.h
@@ -31,10 +31,10 @@ class NativeModuleDefineFuncs {
     /* called on context init */
     void add(const char* module_id, GjsDefineModuleFunc func);
 
-    /* called by importer.c to to check for already loaded modules */
+    // called by importer.cpp to to check for already loaded modules
     [[nodiscard]] bool is_registered(const char* name) const;
 
-    /* called by importer.cpp to load a statically linked native module */
+    // called by importer.cpp to load a built-in native module
     GJS_JSAPI_RETURN_CONVENTION
     bool define(JSContext* cx, const char* name,
                 JS::MutableHandleObject module_out);
