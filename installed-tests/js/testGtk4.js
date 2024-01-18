GNOME/gjs

commit 4760150cd53204240075b53b5b2a956b29d29df3

object: Add special case for destroying GdkSurface
GdkSurface needs to have gdk_surface_destroy() called on it when user code
is finished with it, rather than g_object_unref(). Otherwise, the user
gets a warning about losing the last reference to an undestroyed surface.

GdkSurface should in theory not behave like this, but needs to because of
the X11 backend. See https://gitlab.gnome.org/GNOME/gtk/-/issues/6289.

This adds a workaround which checks, hopefully in a performant way, if the
native GObject pointer to be released is a GdkSurface or subclass. If it
is, we call gdk_surface_destroy() on it (via introspection) rather than
letting the smart pointer's destructor call g_object_unref() on it.

Closes: #592

gi/object.cpp
@@ -1519,11 +1519,38 @@ ObjectInstance::release_native_object(void)
     if (m_gobj_disposed)
         ignore_gobject_finalization();
 
-    if (m_uses_toggle_ref && !m_gobj_disposed)
+    if (m_uses_toggle_ref && !m_gobj_disposed) {
         g_object_remove_toggle_ref(m_ptr.release(), wrapped_gobj_toggle_notify,
                                    this);
-    else
+        return;
+    }
+
+    // Unref the object. Handle any special cases for destruction here
+
+    // Quickest way to check for GdkSurface if Gdk has been loaded?
+    // surface_type may be G_TYPE_NONE if Gdk not loaded. The type may be a
+    // private type and not have introspection info.
+    GType surface_type = g_type_from_name("GdkSurface");
+    if (surface_type && g_type_is_a(gtype(), surface_type)) {
+        GObject* ptr = m_ptr.release();
+
+        // Workaround for https://gitlab.gnome.org/GNOME/gtk/-/issues/6289
+        GjsAutoBaseInfo surface_info =
+            g_irepository_find_by_gtype(nullptr, surface_type);
+        GjsAutoFunctionInfo destroy_func =
+            g_object_info_find_method(surface_info, "destroy");
+        GIArgument destroy_args[1] = {{.v_pointer = ptr}};
+        GIArgument unused_return;
+
+        GjsAutoError err;
+        if (!g_function_info_invoke(destroy_func, destroy_args, 1, nullptr, 0,
+                                    &unused_return, err.out()))
+            g_critical("Error destroying GdkSurface %p: %s", ptr, err->message);
+    } else {
         m_ptr = nullptr;
+    }
+
+    g_assert(!m_ptr && "Forgot to release GObject pointer in special case");
 }
 
 /* At shutdown, we need to ensure we've cleared the context of any

installed-tests/js/testGtk4.js
@@ -270,6 +270,17 @@ describe('Gtk 4 regressions', function () {
         }
         expect(result).toEqual([0, GLib.MAXUINT32]);
     });
+
+    function createSurface() {
+        // Create a Gdk.Surface that is unreachable after this function ends
+        const display = Gdk.Display.get_default();
+        void Gdk.Surface.new_toplevel(display);
+    }
+
+    it('Gdk.Surface is destroyed properly', function () {
+        createSurface();
+        System.gc();
+    });
 });
 
 class LeakTestWidget extends Gtk.Button {

commit 33d43192198a1e1814bbc8820c2d9e3d9efd0134

object: Only destroy GdkSurface if the JS ref is the last one
Fixes a regression from 4760150c. We do not want to destroy a GdkSurface
if other references to it are being held from C.

Closes: #598

gi/object.cpp
@@ -1487,6 +1487,8 @@ void ObjectInstance::wrapped_gobj_toggle_notify(void* instance, GObject*,
 void
 ObjectInstance::release_native_object(void)
 {
+    static GType gdksurface_type = 0;
+
     discard_wrapper();
 
     if (m_gobj_finalized) {
@@ -1512,31 +1514,32 @@ ObjectInstance::release_native_object(void)
     }
 
     // Unref the object. Handle any special cases for destruction here
+    if (m_ptr->ref_count == 1) {
+        // Quickest way to check for GdkSurface if Gdk has been loaded?
+        // surface_type may be 0 if Gdk not loaded. The type may be a private
+        // type and not have introspection info.
+        if (!gdksurface_type)
+            gdksurface_type = g_type_from_name("GdkSurface");
+        if (gdksurface_type && g_type_is_a(gtype(), gdksurface_type)) {
+            GObject* ptr = m_ptr.release();
 
-    // Quickest way to check for GdkSurface if Gdk has been loaded?
-    // surface_type may be G_TYPE_NONE if Gdk not loaded. The type may be a
-    // private type and not have introspection info.
-    GType surface_type = g_type_from_name("GdkSurface");
-    if (surface_type && g_type_is_a(gtype(), surface_type)) {
-        GObject* ptr = m_ptr.release();
+            // Workaround for https://gitlab.gnome.org/GNOME/gtk/-/issues/6289
+            GjsAutoBaseInfo surface_info =
+                g_irepository_find_by_gtype(nullptr, gdksurface_type);
+            GjsAutoFunctionInfo destroy_func =
+                g_object_info_find_method(surface_info, "destroy");
+            GIArgument destroy_args[1] = {{.v_pointer = ptr}};
+            GIArgument unused_return;
 
-        // Workaround for https://gitlab.gnome.org/GNOME/gtk/-/issues/6289
-        GjsAutoBaseInfo surface_info =
-            g_irepository_find_by_gtype(nullptr, surface_type);
-        GjsAutoFunctionInfo destroy_func =
-            g_object_info_find_method(surface_info, "destroy");
-        GIArgument destroy_args[1] = {{.v_pointer = ptr}};
-        GIArgument unused_return;
-
-        GjsAutoError err;
-        if (!g_function_info_invoke(destroy_func, destroy_args, 1, nullptr, 0,
-                                    &unused_return, err.out()))
-            g_critical("Error destroying GdkSurface %p: %s", ptr, err->message);
-    } else {
-        m_ptr = nullptr;
+            GjsAutoError err;
+            if (!g_function_info_invoke(destroy_func, destroy_args, 1, nullptr,
+                                        0, &unused_return, err.out()))
+                g_critical("Error destroying GdkSurface %p: %s", ptr,
+                           err->message);
+        }
     }
 
-    g_assert(!m_ptr && "Forgot to release GObject pointer in special case");
+    m_ptr = nullptr;
 }
 
 /* At shutdown, we need to ensure we've cleared the context of any

installed-tests/js/testGtk4.js
@@ -271,15 +271,24 @@ describe('Gtk 4 regressions', function () {
         expect(result).toEqual([0, GLib.MAXUINT32]);
     });
 
-    function createSurface() {
+    function createSurface(shouldStash) {
         // Create a Gdk.Surface that is unreachable after this function ends
         const display = Gdk.Display.get_default();
-        void Gdk.Surface.new_toplevel(display);
+        const surface = Gdk.Surface.new_toplevel(display);
+        if (shouldStash)
+            GjsTestTools.save_object(surface);
     }
 
     it('Gdk.Surface is destroyed properly', function () {
-        createSurface();
+        createSurface(false);
         System.gc();
+    });
+
+    it('Gdk.Surface is not destroyed if a ref is held from C', function () {
+        createSurface(true);
+        System.gc();
+        const surface = GjsTestTools.steal_saved();
+        expect(surface.is_destroyed()).toBeFalsy();
     });
 });
 