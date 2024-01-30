GNOME/gjs

commit 3fb5157eb63bb7dc8db707be2d33fd758c71ee6a

package: Specify GIRepository version
Now that girepository moved into the glib tree, there's a newer,
incompatible version around. That version will be picked by
default, so explicitly specify the correct version to not break
the module.

modules/script/package.js
@@ -9,6 +9,7 @@ requireSymbol, run, start, version */
  * This module provides a set of convenience APIs for building packaged
  * applications.
  */
+imports.gi.versions.GIRespository = '2.0';
 
 const GLib = imports.gi.GLib;
 const GIRepository = imports.gi.GIRepository;

commit 921c7ff9e345bc34d7183a875e485013e6b9296b

package: Really specify GIRepository version
Ugh, idiots gonna idiot.

Fixes: 3fb5157e ("package: Specify GIRepository version")

modules/script/package.js
@@ -9,7 +9,7 @@ requireSymbol, run, start, version */
  * This module provides a set of convenience APIs for building packaged
  * applications.
  */
-imports.gi.versions.GIRespository = '2.0';
+imports.gi.versions.GIRepository = '2.0';
 
 const GLib = imports.gi.GLib;
 const GIRepository = imports.gi.GIRepository;