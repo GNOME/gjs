07ba3767537a9eebdd49669afe3478d35af004b8
Gary Li <gary.li1@uwaterloo.ca>
2024-07-26 20:51:06 -0400

    tests: add tests for source maps in exceptions logging

---
+++ test/source-maps/inlined/numberWork.js
@@ -0,0 +1,3 @@
+import { get2ndNumber } from "./number.js";
+get2ndNumber([{ n: 1 }, { n: null }]);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibnVtYmVyV29yay5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm51bWJlcldvcmsudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUFFLFlBQVksRUFBRSxNQUFNLGFBQWEsQ0FBQztBQUUzQyxZQUFZLENBQUMsQ0FBQyxFQUFDLENBQUMsRUFBQyxDQUFDLEVBQUMsRUFBRSxFQUFDLENBQUMsRUFBRSxJQUFJLEVBQUMsQ0FBQyxDQUFDLENBQUMifQ==
---
+++ test/source-maps/separate/number.js
@@ -0,0 +1,4 @@
+export var get2ndNumber = function (num) {
+    return num[1].n.toFixed(1);
+};
+//# sourceMappingURL=number.js.map
