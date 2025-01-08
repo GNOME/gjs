698f851ea8e95748a3098364bd6c8682ecaff19a
Gary Li <gary.li1@uwaterloo.ca>
2025-01-08 13:29:27 -0500

    debugger: show private fields in print and keys command
    GJS currently skips any private fields in a class for print and keys. Leverage SM's getOwnPrivateProperties API in the Debugger to retrieve these field values and pass them to the debuggee compartment's prettyPrint function.
    
    Build a tree containing all the private properties of an object as well as any private properties under private/non-private properties of that object.
    
    Fixes https://gitlab.gnome.org/GNOME/gjs/-/issues/455

--- installed-tests/debugger/print.debugger.js
+++ installed-tests/debugger/print.debugger.js
@@ -9,12 +9,54 @@ const e = false;
 const f = true;
 const g = Symbol('foobar');
 const h = [1, 'money', 2, 'show', {three: 'to', 'get ready': 'go cat go'}];
-const i = {some: 'plain object', that: 'has keys'};
+const i = {some: 'plain object', that: 'has keys', with: null, and: undefined};
 const j = new Set([5, 6, 7]);
 const k = class J {};
 const l = new GObject.Object();
 const m = new Error('message');
 const n = {a: 1};
 const o = {some: 'plain object', [Symbol('that')]: 'has symbols'};
+
+class Parent {
+    #privateField;
+    constructor() {
+        this.#privateField = 1;
+    }
+}
+
+class Child extends Parent {
+    #subPrivateField;
+    meaningOfLife = 42;
+    constructor() {
+        super();
+        this.#subPrivateField = 2;
+    }
+}
+
+class PrivateTest extends Child {
+    #child;
+    childVisible;
+    #customToStringChild;
+    #circular1;
+    #circular2;
+    #selfRef;
+    #date;
+    #privateFunc;
+
+    constructor() {
+        super();
+        this.#child = new Child();
+        this.childVisible = new Child();
+        this.#customToStringChild = new Child();
+        this.#customToStringChild.toString = () => 'Custom child!';
+        this.#circular2 = {};
+        this.#circular1 = {n: this.#circular2};
+        this.#circular2.n = this.#circular1;
+        this.#selfRef = this;
+        this.#date = new Date('2025-01-07T00:53:42.417Z');
+        this.#privateFunc = () => 1;
+    }
+}
+const p = new PrivateTest();
 debugger;
-void (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o);
+void (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
