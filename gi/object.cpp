GNOME/gjs

commit 74171bf3ae91ff64337b6261fe024e34e073f86f

object: fix property regression
Fallback to GType system for non custom GObject with no GI information

Fix issue https://gitlab.gnome.org/GNOME/gjs/issues/299

gi/object.cpp
@@ -667,7 +667,28 @@ bool ObjectPrototype::resolve_no_info(JSContext* cx, JS::HandleObject obj,
         canonicalize_key(canonical_name);
     }
 
-    GType *interfaces = g_type_interfaces(m_gtype, &n_interfaces);
+    GjsAutoFree<GType> interfaces = g_type_interfaces(m_gtype, &n_interfaces);
+
+    /* Fallback to GType system for non custom GObjects with no GI information
+     */
+    if (canonical_name && G_TYPE_IS_CLASSED(m_gtype) &&
+        !g_type_get_qdata(m_gtype, ObjectInstance::custom_type_quark())) {
+        GjsAutoTypeClass<GObjectClass> oclass(m_gtype);
+
+        if (g_object_class_find_property(oclass, canonical_name))
+            return lazy_define_gobject_property(cx, obj, id, resolved, name);
+
+        for (i = 0; i < n_interfaces; i++) {
+            if (!G_TYPE_IS_CLASSED(interfaces[i]))
+                continue;
+
+            GjsAutoTypeClass<GObjectClass> iclass(interfaces[i]);
+
+            if (g_object_class_find_property(iclass, canonical_name))
+                return lazy_define_gobject_property(cx, obj, id, resolved, name);
+        }
+    }
+
     for (i = 0; i < n_interfaces; i++) {
         GjsAutoInterfaceInfo iface_info =
             g_irepository_find_by_gtype(nullptr, interfaces[i]);
@@ -678,13 +699,10 @@ bool ObjectPrototype::resolve_no_info(JSContext* cx, JS::HandleObject obj,
             g_interface_info_find_method(iface_info, name);
         if (method_info) {
             if (g_function_info_get_flags (method_info) & GI_FUNCTION_IS_METHOD) {
-                if (!gjs_define_function(cx, obj, m_gtype, method_info)) {
-                    g_free(interfaces);
+                if (!gjs_define_function(cx, obj, m_gtype, method_info))
                     return false;
-                }
 
                 *resolved = true;
-                g_free(interfaces);
                 return true;
             }
         }
@@ -695,14 +713,11 @@ bool ObjectPrototype::resolve_no_info(JSContext* cx, JS::HandleObject obj,
         /* If the name refers to a GObject property, lazily define the property
          * in JS as we do below in the real resolve hook. We ignore fields here
          * because I don't think interfaces can have fields */
-        if (is_ginterface_property_name(iface_info, canonical_name)) {
-            g_free(interfaces);
+        if (is_ginterface_property_name(iface_info, canonical_name))
             return lazy_define_gobject_property(cx, obj, id, resolved, name);
-        }
     }
 
     *resolved = false;
-    g_free(interfaces);
     return true;
 }
 

commit 865330970a55a7046e1488092c2e34eae9db1cff

object: Remove dead code in resolve_no_info()
According to the documentation for G_TYPE_IS_CLASSED, interface GTypes are
not classed. So this loop through the interfaces would always skip every
interface. It's not clear whether this has always been the case, but it
may have been broken since being introduced in 74171bf3.

It doesn't seem to be necessary, because any GObject class implementing a
GInterface must override all the properties from the interface. (If you
try to skip doing that, you'll get a critical in class_init.)

The second dead code block seems like it was necessary in the past, but no
longer now that we resolve on interface prototypes.

Neither of these lazy_define_gobject_property calls are ever hit in any
part of the test suite, according to code coverage.

gi/object.cpp
@@ -1755,22 +1755,6 @@ bool ObjectPrototype::resolve_no_info(JSContext* cx, JS::HandleObject obj,
                 g_object_class_find_property(oclass, canonical_name))
             return lazy_define_gobject_property(cx, obj, id, pspec, resolved,
                                                 name);
-
-        for (const GI::InterfaceInfo& iface_info : interfaces) {
-            GType iface_gtype = iface_info.gtype();
-            if (!G_TYPE_IS_CLASSED(iface_gtype))
-                continue;
-
-            Gjs::AutoTypeClass<GObjectClass> iclass{iface_gtype};
-
-            if (GParamSpec* pspec =
-                    g_object_class_find_property(iclass, canonical_name)) {
-                return lazy_define_gobject_property(
-                    cx, obj, id, pspec, resolved, name,
-                    get_ginterface_property_by_name(iface_info,
-                                                    canonical_name));
-            }
-        }
     }
 
     for (const GI::InterfaceInfo& iface_info : interfaces) {
@@ -1788,25 +1772,6 @@ bool ObjectPrototype::resolve_no_info(JSContext* cx, JS::HandleObject obj,
 
             *resolved = true;
             return true;
-        }
-
-        /* If the name refers to a GObject property, lazily define the property
-         * in JS as we do below in the real resolve hook. We ignore fields here
-         * because I don't think interfaces can have fields */
-        if (canonical_name) {
-            Maybe<const GI::AutoPropertyInfo> prop_info{
-                get_ginterface_property_by_name(iface_info, canonical_name)};
-
-            if (prop_info) {
-                Gjs::AutoTypeClass<GObjectClass> oclass{m_gtype};
-                // unowned
-                GParamSpec* pspec = g_object_class_find_property(
-                    oclass, canonical_name);  // unowned
-                if (pspec && pspec->owner_type == m_gtype) {
-                    return lazy_define_gobject_property(
-                        cx, obj, id, pspec, resolved, name, prop_info);
-                }
-            }
         }
 
         if (!resolve_on_interface_prototype(cx, iface_info, id, obj, resolved))