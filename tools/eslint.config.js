942d0f25e8479ff9fe8457c453c84ba185dca87a
Philip Chimento <philip.chimento@gmail.com>
2025-09-28 17:47:15 -0700

    maint: Ignore subprojects in eslint

--- tools/eslint.config.js
+++ tools/eslint.config.js
@@ -19,6 +19,7 @@ export default defineConfig([
             'modules/internal/source-map/',
             'installed-tests/debugger/sourcemap*',
             'test/source-maps/',
+            'subprojects/',
         ],
     },
     {
