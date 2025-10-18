00c55a60125a61f7f442aee44ad5146cc09a3d03
Philip Chimento <philip.chimento@gmail.com>
2025-10-18 13:53:15 -0700

    maint: Check all shell scripts with shellcheck
    https://gitlab.gnome.org/GNOME/gjs/-/issues/720

--- tools/run_shellcheck.sh
+++ tools/run_shellcheck.sh
@@ -2,4 +2,5 @@
 # SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
 # SPDX-FileCopyrightText: 2025 Marco Trevisan
 
-shellcheck -x $(find installed-tests -type f -name "*.sh" -print0 | sort | xargs -0)
+mapfile -t files < <(find . -not -path "./subprojects/*" -type f -name "*.sh")
+shellcheck -x "${files[@]}"
