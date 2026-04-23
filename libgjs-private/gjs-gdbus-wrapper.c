8a2d7460186bbff331e010c310f540845685e0cc
Philip Chimento <philip.chimento@gmail.com>
2026-04-22 22:05:50 -0700

    dbus: Indicate that GError pointer is moved
    Use g_steal_pointer() to show that g_dbus_method_invocation_take_error()
    takes ownership of the pointer.
    
    Spotted by Goblint.

--- libgjs-private/gjs-gdbus-wrapper.c
+++ libgjs-private/gjs-gdbus-wrapper.c
@@ -111,7 +111,7 @@ static void gjs_dbus_implementation_method_call(
     if (!gjs_dbus_implementation_check_interface(self, connection, object_path,
                                                  interface_name, &error)) {
         g_dbus_method_invocation_take_error(g_steal_pointer(&invocation),
-                                            error);
+                                            g_steal_pointer(&error));
         return;
     }
     if (!G_UNLIKELY(g_dbus_method_invocation_get_method_info(invocation)) ||
