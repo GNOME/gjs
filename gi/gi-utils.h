2daf4158b7ec51a1cee91f4348aa510b1cb43392
Philip Chimento <philip.chimento@gmail.com>
2026-05-11 22:27:19 -0700

    arg: Factor out function for supported GHashTable key types
    JS plain objects can only have integers or strings as keys (or symbols,
    which have no equivalent in the GObject Introspection type system). For
    functions that accept or return a GHashTable, we can currently only
    support a subset of key types.
    
    Move the determination of which key types are supported into gi-utils.h
    so it can be used by the .d.ts generator.

--- gi/gi-utils.h
+++ gi/gi-utils.h
@@ -63,6 +63,43 @@ inline bool is_g_value(const RegisteredTypeInfo& rt) {
 }
 
 [[nodiscard]]
+inline bool is_supported_ghash_key_type(const GITypeTag tag) {
+    switch (tag) {
+        case GI_TYPE_TAG_BOOLEAN:
+        case GI_TYPE_TAG_INT8:
+        case GI_TYPE_TAG_UINT8:
+        case GI_TYPE_TAG_INT16:
+        case GI_TYPE_TAG_UINT16:
+        case GI_TYPE_TAG_INT32:
+        case GI_TYPE_TAG_UINT32:
+        case GI_TYPE_TAG_UTF8:
+        case GI_TYPE_TAG_FILENAME:
+        case GI_TYPE_TAG_UNICHAR:
+            return true;
+        case GI_TYPE_TAG_INT64:
+        case GI_TYPE_TAG_UINT64:
+        case GI_TYPE_TAG_FLOAT:
+        case GI_TYPE_TAG_DOUBLE:
+        // FIXME: The above four could be supported, but are currently not. The
+        // ones below cannot be key types in a regular JS object; we would need
+        // to allow marshalling Map objects into GHashTables to support those,
+        // as well as refactoring this function to take GITypeInfo* and
+        // splitting out the marshalling for basic types into a different
+        // function.
+        case GI_TYPE_TAG_VOID:
+        case GI_TYPE_TAG_GTYPE:
+        case GI_TYPE_TAG_ERROR:
+        case GI_TYPE_TAG_INTERFACE:
+        case GI_TYPE_TAG_GLIST:
+        case GI_TYPE_TAG_GSLIST:
+        case GI_TYPE_TAG_GHASH:
+        case GI_TYPE_TAG_ARRAY:
+            return false;
+    }
+    g_assert_not_reached();
+}
+
+[[nodiscard]]
 inline bool is_supported_gobject_field_type(const TypeInfo& type) {
     switch (type.tag()) {
         case GI_TYPE_TAG_VOID:
