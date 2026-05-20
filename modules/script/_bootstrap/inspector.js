2159fb8f064b0781b51c8dfb596d85db74e57fe8
Angelo Verlain <hey@vixalien.com>
2026-05-20 17:09:23 +0200

    feat: add initial inspector

--- modules/script/_bootstrap/inspector.js
+++ modules/script/_bootstrap/inspector.js
@@ -5,7 +5,74 @@
 
 "use strict";
 
-const { print } = loadNative("_print");
+const { Gio, GLib, GioUnix } = debuggee.imports.gi;
 
-print("GJS inspector bootstrap loaded");
+function encode(str) {
+    const encoder = new TextEncoder();
+    return encoder.encode(str);
+}
+
+function decode(bytes) {
+    const decoder = new TextDecoder();
+    return decoder.decode(bytes);
+}
+
+function readMessage() {
+    const input = Gio.DataInputStream.new(GioUnix.InputStream.new(0, false));
+
+    let contentLength = null;
+
+    while (true) {
+        const [lineBytes] = input.read_line(null);
+
+        if (lineBytes === null) {
+            return null;
+        }
+
+        const line = decode(lineBytes)
+            // handle carriage returns
+            .replace(/\r$/, "");
+
+        if (line == "") {
+            break;
+        }
+
+        const match = /^Content-Length: (\d+)$/i.exec(line);
+
+        if (match !== null) {
+            contentLength = parseInt(match[1]);
+            break;
+        }
+    }
+
+    const bytes = input.read_bytes(contentLength, null);
+    return JSON.parse(decode(bytes));
+}
+
+function sendMessage(message) {
+    const body = encode(JSON.stringify(message));
+    const header = `Content-Length: ${body.length}\r\n\r\n`;
+
+    const output = GioUnix.OutputStream.new(1, false);
+
+    output.write_all(encode(header), null);
+    output.write_all(body, null);
+    output.flush(null);
+}
+
+const request = readMessage();
+
+if (request && request.type === "request" && request.command === "initialize") {
+    sendMessage({
+        seq: 1,
+        type: "response",
+        request_set: request.seq,
+        success: true,
+        command: "initialize",
+        body: {
+            supportsConfigurationDoneRequest: false,
+        },
+    });
+}
+
 quit(0);
---
+++ tools/dap-test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# usage: tools/dap-test.py | build/gjs-console --inspect test.js
+import json
+import sys
+
+message = {
+    "seq": 1,
+    "type": "request",
+    "command": "initialize",
+    "arguments": {"clientID": "manual", "adapterId": "gjs"},
+}
+
+body = json.dumps(message, separators=(",", ":")).encode()
+sys.stdout.buffer.write(f"Content-Length: {len(body)}\r\n\r\n".encode())
+sys.stdout.buffer.write(body)
