GNOME/gjs

commit ccc0812b0d559d71313f28a28bc3ab4b220d6cd4

GLib: Remove unnecessary args spread parameter
This function doesn't take parameters, so there's no need to spread and
unspread them.

modules/core/overrides/GLib.js
@@ -259,11 +259,11 @@ function _init() {
 
     GLib = this;
 
-    GLib.MainLoop.prototype.runAsync = function (...args) {
+    GLib.MainLoop.prototype.runAsync = function () {
         return new Promise((resolve, reject) => {
             setMainLoopHook(() => {
                 try {
-                    resolve(this.run(...args));
+                    resolve(this.run());
                 } catch (error) {
                     reject(error);
                 }

commit f4ac51e6347ac5eb240da3bf0875c451a43f7388

Gio: Restore arguments to Gio.Application.runAsync
Regression from ccc0812b. These two methods don't need to be the same,
anyway. The GLib.MainLoop one doesn't need to spread the arguments or
pass a value to the resolver, and the Gio.Application one should do what
the original code did.

Add a test to ensure this doesn't regress in future.

modules/core/overrides/GLib.js
@@ -263,7 +263,8 @@ function _init() {
         return new Promise((resolve, reject) => {
             setMainLoopHook(() => {
                 try {
-                    resolve(this.run());
+                    this.run();
+                    resolve();
                 } catch (error) {
                     reject(error);
                 }

modules/core/overrides/Gio.js
@@ -5,6 +5,7 @@ var GLib = imports.gi.GLib;
 var GjsPrivate = imports.gi.GjsPrivate;
 var Signals = imports.signals;
 const {_createWrappersForPlatformSpecificNamespace} = imports._common;
+const {setMainLoopHook} = imports._promiseNative;
 var Gio;
 
 // Ensures that a Gio.UnixFDList being passed into or out of a DBus method with
@@ -516,7 +517,17 @@ const _methodInvocations = new WeakMap();
 function _init() {
     Gio = this;
 
-    Gio.Application.prototype.runAsync = GLib.MainLoop.prototype.runAsync;
+    Gio.Application.prototype.runAsync = function (...args) {
+        return new Promise((resolve, reject) => {
+            setMainLoopHook(() => {
+                try {
+                    resolve(this.run(...args));
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        });
+    };
 
     _createWrappersForPlatformSpecificNamespace(Gio);
 

installed-tests/js/testAsyncMainloop.js
@@ -1,26 +1,42 @@
 // SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
 // SPDX-FileCopyrightText: 2022 Evan Welsh <contact@evanwelsh.com>
 
+import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import {acquireMainloop} from 'resource:///org/gjs/jsunit/minijasmine.js';
 
 describe('Async mainloop', function () {
-    let loop, quit;
-
+    let release;
     beforeEach(function () {
-        loop = new GLib.MainLoop(null, false);
-        quit = jasmine.createSpy('quit').and.callFake(() => loop.quit());
+        release = acquireMainloop();
     });
 
     it('resolves when main loop exits', async function () {
-        const release = acquireMainloop();
+        const loop = new GLib.MainLoop(null, false);
+        const quit = jasmine.createSpy('quit').and.callFake(() => loop.quit());
 
         GLib.timeout_add_once(GLib.PRIORITY_DEFAULT, 50, quit);
 
         await loop.runAsync();
 
         expect(quit).toHaveBeenCalled();
+    });
 
+    it('resolves when application exits', async function () {
+        const app = new Gio.Application({
+            applicationId: 'org.gnome.gjs.ExampleApplication',
+            flags: Gio.ApplicationFlags.HANDLES_OPEN,
+        });
+        app.connect('open', (app, [file1, file2]) => {
+            expect(file1.get_basename()).toBe('foo');
+            expect(file2.get_basename()).toBe('bar');
+            app.quit();
+        });
+
+        await app.runAsync(['invocation', 'foo', 'bar']);
+    });
+
+    afterEach(function () {
         release();
     });
 });