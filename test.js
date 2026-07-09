e0919112546a6f8e7f91afd8e924916573c004c4
Angelo Verlain <hey@vixalien.com>
2026-07-09 15:11:39 +0200

    chore: support pause on initial step

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-function report(what) {
-    console.log("clicked: " + what);
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-report("Hello");
+for (let i = 0; i < 60; i++) {
+    await sleep(1_000);
+    console.log(`Iteration ${i}`);
+}
