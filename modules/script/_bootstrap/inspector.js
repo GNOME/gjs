96a5c1169ee522c91640f28a4cb1e40b3c26fcd8
Angelo Verlain <hey@vixalien.com>
2026-08-10 18:20:38 +0200

    chore: inspector

--- modules/script/_bootstrap/inspector.js
+++ modules/script/_bootstrap/inspector.js
@@ -1,11 +1,40 @@
+// oxlint-disable no-unused-vars
 /* -*- indent-tabs-mode: nil; js-indent-level: 4 -*- */
 /* global debuggee, quit, loadNative, readline, uneval, launchFile */
 // SPDX-License-Identifier: MPL-2.0
 // SPDX-FileCopyrightText: 2026 Angelo Verlain
 
+// @ts-check
+/// <reference path="./types.d.ts" />
+
 const { print, printerr } = loadNative('_print');
 const Encoding = loadNative('_encodingNative');
 
+// DEBUGGER STATE
+
+/**
+ * @typedef {Object} BreakpointEntry
+ * @property {number} id
+ * @property {number} line
+ * @property {boolean} verified
+ */
+
+const STATE = {
+    messageIdSeq: 0,
+    paused: false,
+    breakpointIdSeq: 0,
+    /** @type {Map<string, BreakpointEntry[]>} */
+    pendingBreakpoints: new Map(),
+    requestIdSeq: 0,
+    /** @type {string | null} */
+    pendingLaunchPath: null,
+};
+
+// UTILITY FUNCTIONS
+
+/**
+ * @param {string} str
+ */
 function encode(str) {
     if (str == '') return new Uint8Array();
     return Encoding.encode(str, 'utf8');
@@ -15,7 +44,7 @@ const STDIN = 0;
 const input = openInputStream(STDIN);
 
 function readMessage() {
-    let contentLength = null;
+    let contentLength = 0;
 
     while (true) {
         const line = readLine(input);
@@ -32,67 +61,92 @@ function readMessage() {
     return JSON.parse(body);
 }
 
-let REQUEST_SEQ = 0;
-let pendingLaunchPath = null;
-
+/**
+ * @param {Omit<DAP.ProtocolMessage, 'seq'>} message
+ */
 function sendMessage(message) {
-    const newSeq = ++REQUEST_SEQ;
+    const newSeq = ++STATE.requestIdSeq;
 
     const body = JSON.stringify({ seq: newSeq, ...message });
     // Add extra length bytes for \r\n at the end of body. print() auto-adds \n
     print(`Content-Length: ${encode(body).length + 2}\r\n\r\n${body}\r`);
 }
 
-function sendResponse(command, body, requestSeq) {
-    sendMessage({
+/**
+ * @param {number} requestSeq
+ * @param {string} command
+ * @param {any} [body]
+ */
+function sendResponse(requestSeq, command, body) {
+    /** @type {Omit<DAP.Response, 'seq'>} */
+    const response = {
         type: 'response',
         success: true,
         command,
         request_seq: requestSeq,
         body,
-    });
+    };
+
+    sendMessage(response);
 }
 
-function newMessage(id, format) {
+/**
+ * @param {string} format
+ * @returns {DAP.Message}
+ */
+function newMessage(format) {
     return {
-        id,
+        id: STATE.messageIdSeq++,
         format,
     };
 }
 
 /**
- *
+ * @param {any} requestSeq
  * @param {string} command
  * @param {ReturnType<typeof newMessage>} error
  */
-function sendErrorResponse(command, error, requestSeq) {
-    sendMessage({
+function sendErrorResponse(requestSeq, command, error) {
+    /** @type {Omit<DAP.ErrorResponse, 'seq'>} */
+    const response = {
         type: 'response',
         success: false,
         command,
         request_seq: requestSeq,
         body: { error },
-    });
+    };
+    sendMessage(response);
 }
 
+/**
+ * @param {string} type
+ * @param {any} body
+ */
 function sendEvent(type, body = {}) {
-    sendMessage({
+    /** @type {Omit<DAP.Event, 'seq'>} */
+    const event = {
         type: 'event',
         event: type,
         body,
-    });
+    };
+
+    sendMessage(event);
 }
 
+// COMMAND HANDLERS
+
+/** @type {Record<string, (seq: number, args: any) => void>} */
 const handlers = {
-    initialize(args, seq) {
-        sendResponse(
-            'initialize',
-            { supportsConfigurationDoneRequest: true },
-            seq,
-        );
+    initialize(seq) {
+        sendResponse(seq, 'initialize', {
+            supportsConfigurationDoneRequest: true,
+        });
         sendEvent('initialized');
     },
-    launch(args, seq) {
+    /**
+     * @param {{ cwd: string; program: string; stopOnEntry: boolean; }} args
+     */
+    launch(seq, args) {
         const cwd = args.cwd || '.';
         const filePath = cwd + '/' + args.program;
 
@@ -100,26 +154,21 @@ const handlers = {
             dbg.onEnterFrame = onInitialEnterFrame;
         }
 
-        pendingLaunchPath = filePath;
-        sendResponse('launch', undefined, seq);
+        STATE.pendingLaunchPath = filePath;
+        sendResponse(seq, 'launch');
     },
-    setExceptionBreakpoints(args, seq) {
-        /**
-        TODO: Currently no-op
-
-        {
-          "filters": [],
-          "filterOptions": []
-        }
-        */
-        sendResponse('setExceptionBreakpoints', undefined, seq);
+    /**
+     * @param {{filters: any[]; filterOptions: any[]}} _args
+     */
+    setExceptionBreakpoints(seq, _args) {
+        sendResponse(seq, 'setExceptionBreakpoints');
     },
-    configurationDone(args, seq) {
-        sendResponse('configurationDone', undefined, seq);
+    configurationDone(seq) {
+        sendResponse(seq, 'configurationDone');
 
-        if (pendingLaunchPath) {
+        if (STATE.pendingLaunchPath) {
             try {
-                launchFile(pendingLaunchPath);
+                launchFile(STATE.pendingLaunchPath);
 
                 sendEvent('exited', { exitCode: 0 });
                 sendEvent('terminated');
@@ -131,36 +180,46 @@ const handlers = {
                 sendEvent('exited', { exitCode: 1 });
                 sendEvent('terminated');
                 quit(1);
+            } finally {
+                STATE.pendingLaunchPath = null;
             }
-            pendingLaunchPath = null;
         }
     },
-    disconnect(args, seq) {
-        sendResponse('disconnect', undefined, seq);
+    disconnect(seq) {
+        sendResponse(seq, 'disconnect');
         quit(0);
     },
-    attach(args, seq) {
+    attach(seq) {
         sendErrorResponse(
-            'attach',
-            newMessage(0, 'GJS does not support attach mode'),
             seq,
+            'attach',
+            newMessage('GJS does not support attach mode'),
         );
     },
-    threads(args, seq) {
-        sendResponse('threads', { threads: [{ id: 0, name: 'main' }] }, seq);
+    threads(seq) {
+        sendResponse(seq, 'threads', { threads: [{ id: 0, name: 'main' }] });
     },
-    continue(args, seq) {
-        sendResponse('continue', { allThreadsContinued: true }, seq);
-        paused = false;
+    continue(seq) {
+        sendResponse(seq, 'continue', { allThreadsContinued: true });
+        STATE.paused = false;
     },
-    stackTrace(args, seq) {
-        sendResponse('stackTrace', { stackFrames: [], totalFrames: 0 }, seq);
+    stackTrace(seq) {
+        const newestFrame = dbg.getNewestFrame();
+        const dapFrames = toDapStackFrames(newestFrame);
+        printerr(JSON.stringify(dapFrames, null, 2));
+        sendResponse(seq, 'stackTrace', {
+            stackFrames: dapFrames,
+            totalFrames: dapFrames.length,
+        });
     },
-    pause(args, seq) {
-        sendResponse('pause', undefined, seq);
+    pause(seq) {
+        sendResponse(seq, 'pause');
         pause();
     },
-    setBreakpoints(args, seq) {
+    /**
+     * @param {{ source: { path: string; name: string; }; breakpoints: {line: number}[]; }} args
+     */
+    setBreakpoints(seq, args) {
         /***
         "source": {
            "name": "test.js",
@@ -173,31 +232,85 @@ const handlers = {
          ],
          "sourceModified": false
          */
-        printerr(JSON.stringify(args));
-        // const scripts = dbg.findScripts({ url: `file://${args.source.path}` });
-        const scripts = dbg.findScripts();
-        printerr("Found n scripts:" + JSON.stringify(scripts))
 
-        scripts.forEach((script, i) => {
-            printerr(i, script.url);
-            printerr(i, script.startLine);
-        })
+        const url = `file://${args.source.path}`;
 
-        sendResponse('setBreakpoints', { breakpoints: [] }, seq);
+        clearBreakpointsForUrl(url);
+
+        const entries = (args.breakpoints || []).map(({ line }) => {
+            return {
+                id: ++STATE.breakpointIdSeq,
+                line,
+                verified: false,
+            };
+        });
+
+        STATE.pendingBreakpoints.set(url, entries);
+
+        // in case the script is already loaded, resolve the breakpoints immediately
+        resolveBreakpointsForUrl(url);
+
+        sendResponse(seq, 'setBreakpoints', {
+            breakpoints: entries,
+        });
     },
 };
 
+/**
+ * @param {Debugger.Frame | null} frame
+ */
+function toDapStackFrame(frame) {
+    if (!frame) return null;
+
+    if (!frame.script) return null;
+
+    const offset = frame.offset
+        ? frame.script.getOffsetLocation(frame.offset)
+        : null;
+
+    printerr('frame props', offset);
+
+    return {
+        id: frame.depth ?? 0,
+        name: frame.script.displayName ?? "helloworld",
+        line: offset?.lineNumber,
+        column: offset?.columnNumber,
+        source: {
+            name: frame.script.url,
+            path: frame.script.url,
+            sourceReference: 0,
+        },
+        presentationHint: 'normal',
+        canRestart: frame.onStack,
+    };
+}
+
+/**
+ * @param {Debugger.Frame | null} frame
+ */
+function toDapStackFrames(frame) {
+    const dapFrames = [];
+
+    while (frame) {
+        dapFrames.push(toDapStackFrame(frame));
+        frame = frame.older;
+    }
+
+    return dapFrames.filter(Boolean);
+}
+
 function _handleRequest() {
+    /** @type {DAP.Request | null} */
     const request = readMessage();
     if (request === null) return false;
 
     const handler = handlers[request.command];
     if (handler === undefined) {
-        // TODO: use the error message
+        // TODO: use the error event
         throw new Error(`Unknown request command: ${request.command}`);
     }
 
-    handler(request.arguments, request.seq);
+    handler(request.seq, request.arguments);
     return true;
 }
 
@@ -208,13 +321,11 @@ function handleRequests(shouldContinue = () => true) {
 }
 
 function pause() {
-    paused = true;
-    handleRequests(() => paused);
+    STATE.paused = true;
+    handleRequests(() => STATE.paused);
 }
 
-// State
-
-let paused = false;
+// DEBUGGER API handlers
 
 function onInitialEnterFrame() {
     // printerr("entered frame", frame?.callee.name);
@@ -229,9 +340,82 @@ function onInitialEnterFrame() {
     pause();
 }
 
+/**
+ * @param {string} url
+ */
+function resolveBreakpointsForUrl(url) {
+    const entries = STATE.pendingBreakpoints.get(url);
+    if (!entries) return;
+
+    const scripts = dbg.findScripts({ url });
+    if (scripts.length === 0) return;
+
+    entries.forEach((entry) => {
+        if (entry.verified) return;
+
+        const matches = scripts
+            .map((script) => {
+                return {
+                    script,
+                    offsets: script.getLineOffsets(entry.line),
+                };
+            })
+            .filter(({ offsets }) => offsets.length !== 0);
+
+        if (matches.length === 0) return;
+
+        matches.forEach(({ script, offsets }) => {
+            entry.verified = true;
+
+            offsets.forEach((offset) =>
+                script.setBreakpoint(offset, new BreakpointHandler(entry)),
+            );
+
+            sendEvent('breakpoint', {
+                reason: 'changed',
+                breakpoint: entry,
+            });
+        });
+    });
+}
+
+/**
+ * @param {string} url
+ */
+function clearBreakpointsForUrl(url) {
+    dbg.findScripts({ url }).forEach((script) => script.clearAllBreakpoints());
+    STATE.pendingBreakpoints.delete(url);
+}
+
+class BreakpointHandler {
+    /**
+     * @param {BreakpointEntry} entry
+     */
+    constructor(entry) {
+        this.entry = entry;
+    }
+
+    /**
+     * @param {Debugger.Frame} frame
+     */
+    hit(frame) {
+        sendEvent('stopped', {
+            reason: 'breakpoint',
+            threadId: 0,
+            allThreadsStopped: true,
+            hitBreakpointIds: [this.entry.id],
+        });
+        pause();
+    }
+}
+
 // Debugger
 
 const dbg = new Debugger();
+
+dbg.onNewScript = (/** @type {Debugger.Script} */ script) => {
+    resolveBreakpointsForUrl(script.url);
+};
 
 const debuggeeGlobalWrapper = dbg.addDebuggee(debuggee);
 
