2f83f65dacf192a2bb5dcd9023ccf47d1e7ada15
Angelo Verlain <hey@vixalien.com>
2026-09-05 11:58:31 +0200

    chore: remove unnecessary files

--- test.js
+++
@@ -1,8 +0,0 @@
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-for (let i = 0; i < 60; i++) {
-    await sleep(1_000);
-    console.log(`Iteration ${i}`);
-}
