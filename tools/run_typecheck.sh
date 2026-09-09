9bad72c6479ddcef1be652e5764d3e8ae0da24f9
Philip Chimento <philip.chimento@gmail.com>
2026-09-08 22:21:56 -0700

    CI: Create TypeScript sub-configuration for bootstrap files
    The toplevel tsconfig.json includes GJS's global types (gjs.d.ts) in every
    file. That should not happen for the bootstrap files. Create a subdir
    tsconfig.json that overrides the relevant properties from the toplevel
    config file.

--- tools/run_typecheck.sh
+++ tools/run_typecheck.sh
@@ -6,4 +6,9 @@ export NODE_OPTIONS=--dns-result-order=ipv4first
 
 cd "$(dirname -- "$0")" || exit 1
 npm ci
-npm run typecheck -- "$@"
+
+there_were_errors=0
+for project in .. ../modules/script/_bootstrap; do
+    npm run typecheck -- --project "$project" "$@" || there_were_errors=1
+done
+exit $there_were_errors
