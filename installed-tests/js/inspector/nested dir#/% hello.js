25dc3cd4281b0fcdd80256bd779d19f832d3c884
Angelo Verlain <hey@vixalien.com>
2026-09-21 10:26:20 +0200

    chore: attribute hello.js to me

--- installed-tests/js/inspector/nested dir#/% hello.js
+++ installed-tests/js/inspector/nested dir#/% hello.js
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
-// SPDX-FileCopyrightText: 2026 Philip Chimento <philip.chimento@gmail.com>
+// SPDX-FileCopyrightText: 2026 Angelo Verlain <hey@vixalien.com>
 // eslint-disable-next-line no-debugger
 debugger;
 console.log('Hello from nested dir#/% hello.js');
